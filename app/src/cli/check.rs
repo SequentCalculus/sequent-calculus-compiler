d17aec54dd26b89cdab0a0281498c456d8c55919
David Binder <david.binder@uni-tuebingen.de>
2024-08-05 17:41:55 +0200

    Add CLI interface

---
+++ app/src/cli/check.rs
@@ -0,0 +1,32 @@
+use std::path::PathBuf;
+
+use std::{fs, process};
+
+use fun::parser::fun::ProgParser;
+use fun::syntax::Prog;
+use fun::types::{infer_types, Ty};
+
+#[derive(clap::Args)]
+pub struct Args {
+    filepath: PathBuf,
+}
+
+pub fn exec(cmd: Args) {
+    let content = fs::read_to_string(cmd.filepath).expect("Should have been able to read the file");
+    dispatch(content)
+}
+
+fn dispatch(arg: String) {
+    let parser: ProgParser = ProgParser::new();
+    let parsed: Prog<()> = match parser.parse(&arg) {
+        Ok(tm) => tm,
+        Err(err) => {
+            print!("{}\n", err);
+            process::exit(0)
+        }
+    };
+
+    let m_prog_typed: fun::syntax::Prog<Ty> = infer_types(parsed).unwrap();
+
+    print!("{}", m_prog_typed);
+}
