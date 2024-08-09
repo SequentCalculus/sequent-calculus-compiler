dff5feb69169372d9e50e0590c7ec89e11591f95
MarcoTz <marco.tzschentke@gmail.com>
2024-08-09 11:41:12 +0200

    added fun tests

--- lang/fun2core/src/terms/fun_call.rs
+++ lang/fun2core/src/terms/fun_call.rs
@@ -23,3 +23,108 @@ impl CompileWithCont for fun::syntax::Fun {
         .into()
     }
 }
+
+#[cfg(test)]
+mod compile_tests {
+    use crate::definition::CompileWithCont;
+    use std::rc::Rc;
+
+    fn example_fac() -> fun::syntax::Fun {
+        fun::syntax::Fun {
+            name: "fac".to_owned(),
+            args: vec![fun::syntax::Term::Lit(3)],
+            coargs: vec![],
+        }
+    }
+    fn example_swap() -> fun::syntax::Fun {
+        fun::syntax::Fun {
+            name: "swap".to_owned(),
+            args: vec![fun::syntax::Constructor {
+                id: fun::syntax::Ctor::Tup,
+                args: vec![fun::syntax::Term::Lit(1), fun::syntax::Term::Lit(2)],
+            }
+            .into()],
+            coargs: vec![],
+        }
+    }
+    fn example_multfast() -> fun::syntax::Fun {
+        fun::syntax::Fun {
+            name: "multFast".to_owned(),
+            args: vec![fun::syntax::Constructor {
+                id: fun::syntax::Ctor::Nil,
+                args: vec![],
+            }
+            .into()],
+            coargs: vec!["a0".to_owned()],
+        }
+    }
+
+    #[test]
+    fn compile_fac() {
+        let result = example_fac().compile_opt(&mut Default::default());
+        let expected = core::syntax::Mu {
+            covariable: "a0".to_owned(),
+            statement: Rc::new(
+                core::syntax::Fun {
+                    name: "fac".to_owned(),
+                    producers: vec![core::syntax::Literal { lit: 3 }.into()],
+                    consumers: vec![core::syntax::Consumer::Covar("a0".to_owned())],
+                }
+                .into(),
+            ),
+        }
+        .into();
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_swap() {
+        let result = example_swap().compile_opt(&mut Default::default());
+        let expected = core::syntax::Mu {
+            covariable: "a0".to_owned(),
+            statement: Rc::new(
+                core::syntax::Fun {
+                    name: "swap".to_owned(),
+                    producers: vec![core::syntax::Constructor {
+                        id: core::syntax::Ctor::Tup,
+                        producers: vec![
+                            core::syntax::Literal { lit: 1 }.into(),
+                            core::syntax::Literal { lit: 2 }.into(),
+                        ],
+                        consumers: vec![],
+                    }
+                    .into()],
+                    consumers: vec![core::syntax::Consumer::Covar("a0".to_owned())],
+                }
+                .into(),
+            ),
+        }
+        .into();
+        assert_eq!(result, expected)
+    }
+    #[test]
+    fn compile_multfast() {
+        let result = example_multfast().compile_opt(&mut Default::default());
+        let expected = core::syntax::Mu {
+            covariable: "a0".to_owned(),
+            statement: Rc::new(
+                core::syntax::Fun {
+                    name: "multFast".to_owned(),
+                    producers: vec![core::syntax::Constructor {
+                        id: core::syntax::Ctor::Nil,
+                        producers: vec![],
+                        consumers: vec![],
+                    }
+                    .into()],
+                    consumers: vec![
+                        core::syntax::Consumer::Covar("a0".to_owned()),
+                        core::syntax::Consumer::Covar("a0".to_owned()),
+                    ],
+                }
+                .into(),
+            ),
+        }
+        .into();
+        assert_eq!(result, expected)
+    }
+}
