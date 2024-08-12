39d2936b0c4d8ace976dfafdeea63a502e4e0bbe
MarcoTz <marco.tzschentke@gmail.com>
2024-08-12 11:26:00 +0200

    added test for op

--- lang/fun2core/src/terms/op.rs
+++ lang/fun2core/src/terms/op.rs
@@ -20,3 +20,94 @@ impl CompileWithCont for fun::syntax::Op {
         .into()
     }
 }
+
+#[cfg(test)]
+mod compile_tests {
+    use crate::definition::CompileWithCont;
+    use std::rc::Rc;
+
+    fn example_op1() -> fun::syntax::Op {
+        fun::syntax::Op {
+            fst: Rc::new(fun::syntax::Term::Lit(2)),
+            op: fun::syntax::BinOp::Sub,
+            snd: Rc::new(fun::syntax::Term::Lit(1)),
+        }
+    }
+    fn example_op2() -> fun::syntax::Op {
+        fun::syntax::Op {
+            fst: Rc::new(fun::syntax::Term::Var("x".to_owned())),
+            op: fun::syntax::BinOp::Prod,
+            snd: Rc::new(
+                fun::syntax::Op {
+                    fst: Rc::new(fun::syntax::Term::Var("x".to_owned())),
+                    op: fun::syntax::BinOp::Sub,
+                    snd: Rc::new(fun::syntax::Term::Lit(1)),
+                }
+                .into(),
+            ),
+        }
+    }
+
+    #[test]
+    fn compile_op1() {
+        let result = example_op1().compile_opt(&mut Default::default());
+        let expected = core::syntax::Mu {
+            covariable: "a0".to_owned(),
+            statement: Rc::new(
+                core::syntax::Op {
+                    fst: Rc::new(core::syntax::Literal { lit: 2 }.into()),
+                    op: core::syntax::BinOp::Sub,
+                    snd: Rc::new(core::syntax::Literal { lit: 1 }.into()),
+                    continuation: Rc::new(core::syntax::Consumer::Covar("a0".to_owned())),
+                }
+                .into(),
+            ),
+        }
+        .into();
+        assert_eq!(result, expected);
+    }
+    #[test]
+    fn compile_op2() {
+        let result = example_op2().compile_opt(&mut Default::default());
+        let expected = core::syntax::Mu {
+            covariable: "a0".to_owned(),
+            statement: Rc::new(
+                core::syntax::Op {
+                    fst: Rc::new(
+                        core::syntax::Variable {
+                            var: "x".to_owned(),
+                        }
+                        .into(),
+                    ),
+                    op: core::syntax::BinOp::Prod,
+                    snd: Rc::new(
+                        core::syntax::Mu {
+                            covariable: "a1".to_owned(),
+                            statement: Rc::new(
+                                core::syntax::Op {
+                                    fst: Rc::new(
+                                        core::syntax::Variable {
+                                            var: "x".to_owned(),
+                                        }
+                                        .into(),
+                                    ),
+                                    op: core::syntax::BinOp::Sub,
+                                    snd: Rc::new(core::syntax::Literal { lit: 1 }.into()),
+                                    continuation: Rc::new(core::syntax::Consumer::Covar(
+                                        "a1".to_owned(),
+                                    )),
+                                }
+                                .into(),
+                            ),
+                        }
+                        .into(),
+                    ),
+                    continuation: Rc::new(core::syntax::Consumer::Covar("a0".to_owned())),
+                }
+                .into(),
+            ),
+        }
+        .into();
+        assert_eq!(result, expected);
+    }
+}
