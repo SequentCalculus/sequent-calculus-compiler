3e6b5b48d34422e9ffc8a70d4338268fbfc77cfd
David Binder <david.binder@uni-tuebingen.de>
2024-08-13 14:05:32 +0200

    Fix pretty-printing of fun programs

--- lang/fun/src/program.rs
+++ lang/fun/src/program.rs
@@ -46,7 +46,7 @@ impl<T> fmt::Display for Prog<T> {
             .iter()
             .map(|x| x.to_string())
             .collect::<Vec<String>>()
-            .join(", ");
+            .join("\n");
         write!(f, "{}", defs_joined)
     }
 }
@@ -56,32 +56,12 @@ mod prog_tests {
     use super::{Def, Prog, Term};
     use crate::parser::fun;
 
+    // Empty program
+    //
+    //
+
     fn example_empty() -> Prog<()> {
         Prog { prog_defs: vec![] }
-    }
-
-    fn example_simple() -> Prog<()> {
-        Prog {
-            prog_defs: vec![Def {
-                name: "x".to_string(),
-                args: vec![],
-                cont: vec![],
-                body: Term::Lit(4),
-                ret_ty: (),
-            }],
-        }
-    }
-
-    fn example_args() -> Prog<()> {
-        Prog {
-            prog_defs: vec![Def {
-                name: "f".to_string(),
-                args: vec![("x".to_string(), ())],
-                cont: vec![("a".to_string(), ())],
-                body: Term::Lit(4),
-                ret_ty: (),
-            }],
-        }
     }
 
     #[test]
@@ -93,6 +73,22 @@ mod prog_tests {
     fn parse_empty() {
         let parser = fun::ProgParser::new();
         assert_eq!(parser.parse(" "), Ok(example_empty().into()));
+    }
+
+    // Program with one definition without arguments
+    //
+    //
+
+    fn example_simple() -> Prog<()> {
+        Prog {
+            prog_defs: vec![Def {
+                name: "x".to_string(),
+                args: vec![],
+                cont: vec![],
+                body: Term::Lit(4),
+                ret_ty: (),
+            }],
+        }
     }
 
     #[test]
@@ -109,6 +105,22 @@ mod prog_tests {
         assert_eq!(parser.parse("def x(;) := 4;"), Ok(example_simple().into()));
     }
 
+    // Program with one definition which takes arguments
+    //
+    //
+
+    fn example_args() -> Prog<()> {
+        Prog {
+            prog_defs: vec![Def {
+                name: "f".to_string(),
+                args: vec![("x".to_string(), ())],
+                cont: vec![("a".to_string(), ())],
+                body: Term::Lit(4),
+                ret_ty: (),
+            }],
+        }
+    }
+
     #[test]
     fn display_args() {
         assert_eq!(
@@ -121,5 +133,47 @@ mod prog_tests {
     fn parse_args() {
         let parser = fun::ProgParser::new();
         assert_eq!(parser.parse("def f(x;a) := 4;"), Ok(example_args().into()))
+    }
+
+    // Program with two definitions
+    //
+    //
+
+    fn example_two() -> Prog<()> {
+        let d1 = Def {
+            name: "f".to_string(),
+            args: vec![],
+            cont: vec![],
+            body: Term::Lit(2),
+            ret_ty: (),
+        };
+
+        let d2 = Def {
+            name: "g".to_string(),
+            args: vec![],
+            cont: vec![],
+            body: Term::Lit(4),
+            ret_ty: (),
+        };
+        Prog {
+            prog_defs: vec![d1, d2],
+        }
+    }
+
+    #[test]
+    fn display_two() {
+        assert_eq!(
+            format!("{}", example_two()),
+            "def f(;) := 2;\ndef g(;) := 4;".to_string(),
+        )
+    }
+
+    #[test]
+    fn parse_two() {
+        let parser = fun::ProgParser::new();
+        assert_eq!(
+            parser.parse("def f(;) := 2;\n def g(;) := 4;"),
+            Ok(example_two().into())
+        )
     }
 }
