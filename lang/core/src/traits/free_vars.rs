3722b125d0cf6c0448700f7c322ff1cbd8777396
mm0821 <mari.mueller@uni-tuebingen.de>
2024-08-13 15:17:56 +0200

    Refactor fun a bit

--- lang/core/src/traits/free_vars.rs
+++ lang/core/src/traits/free_vars.rs
@@ -1,5 +1,4 @@
 use std::collections::HashSet;
-use std::rc::Rc;
 
 use crate::syntax::{Covar, Var};
 
@@ -13,24 +12,16 @@ pub trait FreeV {
 
 impl<T: FreeV> FreeV for Vec<T> {
     fn free_vars(self: &Vec<T>) -> HashSet<Var> {
-        self.iter().fold(HashSet::new(), |free_vars, t| {
-            free_vars.union(&t.free_vars()).cloned().collect()
+        self.iter().fold(HashSet::new(), |mut free_vars, t| {
+            free_vars.extend(t.free_vars());
+            free_vars
         })
     }
     fn free_covars(self: &Vec<T>) -> HashSet<Covar> {
-        self.iter().fold(HashSet::new(), |free_vars, t| {
-            free_vars.union(&t.free_vars()).cloned().collect()
+        self.iter().fold(HashSet::new(), |mut free_vars, t| {
+            free_vars.extend(t.free_vars());
+            free_vars
         })
-    }
-}
-
-impl<T: FreeV> FreeV for Rc<T> {
-    fn free_vars(&self) -> HashSet<Var> {
-        (**self).free_vars()
-    }
-
-    fn free_covars(&self) -> HashSet<Covar> {
-        (**self).free_covars()
     }
 }
 
@@ -38,24 +29,24 @@ pub fn fresh_var(xs: &HashSet<Var>) -> Var {
     fresh_var_n(xs, 0)
 }
 
-fn fresh_var_n(xs: &HashSet<Var>, n: i32) -> Var {
-    let new_var: Var = format!("x{}", n);
-    if xs.contains(&new_var) {
-        fresh_var_n(xs, n + 1)
-    } else {
-        new_var
+fn fresh_var_n(xs: &HashSet<Var>, mut n: i32) -> Var {
+    let mut new_var: Var = format!("x{}", n);
+    while xs.contains(&new_var) {
+        n += 1;
+        new_var = format!("x{}", n);
     }
+    new_var
 }
 
 pub fn fresh_covar(xs: &HashSet<Covar>) -> Covar {
     fresh_covar_n(xs, 0)
 }
 
-fn fresh_covar_n(xs: &HashSet<Covar>, n: i32) -> Covar {
-    let new_covar: Covar = format!("a{}", n);
-    if xs.contains(&new_covar) {
-        fresh_covar_n(xs, n + 1)
-    } else {
-        new_covar
+fn fresh_covar_n(xs: &HashSet<Covar>, mut n: i32) -> Covar {
+    let mut new_covar: Covar = format!("a{}", n);
+    while xs.contains(&new_covar) {
+        n += 1;
+        new_covar = format!("a{}", n);
     }
+    new_covar
 }
