04035b7122c8d12f542345c9ebedac1034972df0
MarcoTz <marco.tzschentke@gmail.com>
2024-08-14 17:37:11 +0200

    added test for clause

--- lang/core/src/transform/clause.rs
+++ lang/core/src/transform/clause.rs
@@ -15,3 +15,74 @@ impl<T> NamingTransformation for Clause<T> {
         }
     }
 }
+
+#[cfg(test)]
+mod transform_tests {
+    use crate::{
+        naming_transformation::NamingTransformation,
+        syntax::{Clause, Covariable, Ctor, Cut, Dtor, Variable},
+    };
+    use std::rc::Rc;
+
+    fn example_clause1() -> Clause<Ctor> {
+        Clause {
+            xtor: Ctor::Tup,
+            vars: vec!["x".to_owned(), "y".to_owned()],
+            covars: vec!["a".to_owned()],
+            rhs: Rc::new(
+                Cut {
+                    producer: Rc::new(
+                        Variable {
+                            var: "x".to_owned(),
+                        }
+                        .into(),
+                    ),
+                    consumer: Rc::new(
+                        Covariable {
+                            covar: "a".to_owned(),
+                        }
+                        .into(),
+                    ),
+                }
+                .into(),
+            ),
+        }
+    }
+    fn example_clause2() -> Clause<Dtor> {
+        Clause {
+            xtor: Dtor::Ap,
+            vars: vec!["x".to_owned()],
+            covars: vec!["a".to_owned()],
+            rhs: Rc::new(
+                Cut {
+                    producer: Rc::new(
+                        Variable {
+                            var: "x".to_owned(),
+                        }
+                        .into(),
+                    ),
+                    consumer: Rc::new(
+                        Covariable {
+                            covar: "a".to_owned(),
+                        }
+                        .into(),
+                    ),
+                }
+                .into(),
+            ),
+        }
+    }
+
+    #[test]
+    fn transform_clause1() {
+        let result = example_clause1().transform(&mut Default::default());
+        let expected = example_clause1();
+        assert_eq!(result, expected)
+    }
+    #[test]
+    fn transform_clause2() {
+        let result = example_clause2().transform(&mut Default::default());
+        let expected = example_clause2();
+        assert_eq!(result, expected)
+    }
+}
