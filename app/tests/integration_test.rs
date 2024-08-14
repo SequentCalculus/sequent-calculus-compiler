d9e5e74c7bf28393d27300cb42c61468f29fe29b
David Binder <david.binder@uni-tuebingen.de>
2024-08-14 15:03:07 +0200

    Add integration test for CLI

---
+++ app/tests/integration_test.rs
@@ -0,0 +1,28 @@
+use assert_cmd::Command;
+
+/// The name of the CLI binary
+const BINARY: &str = env!("CARGO_BIN_EXE_grokking");
+
+/// Check that "grokking --version" works correctly
+#[test]
+fn version_command() {
+    let mut cmd = Command::cargo_bin(BINARY).unwrap();
+    let assert = cmd.arg("--version").assert();
+    assert.success().stdout("grokking 0.1.0\n");
+}
+
+/// Check that "grokking check" works correctly
+#[test]
+fn check_command() {
+    let mut cmd = Command::cargo_bin(BINARY).unwrap();
+    let assert = cmd.args(vec!["check", "../examples/Tuples.sc"]).assert();
+    assert.success();
+}
+
+/// Check that "grokking compile" works correctly
+#[test]
+fn compile_command() {
+    let mut cmd = Command::cargo_bin(BINARY).unwrap();
+    let assert = cmd.args(vec!["compile", "../examples/Tuples.sc"]).assert();
+    assert.success();
+}
