2e8c488c237e36c240bf06adcc87c86a805d0149
MarcoTz <marco.tzschentke@gmail.com>
2024-08-15 12:32:55 +0200

    added mutilde test

--- lang/core/src/transform/mutilde.rs
+++ lang/core/src/transform/mutilde.rs
@@ -36,3 +36,86 @@ impl Bind for MuTilde {
         .into()
     }
 }
+
+#[cfg(test)]
+mod transform_tests {
+    use crate::{
+        naming_transformation::{Bind, NamingTransformation},
+        syntax::{Covariable, Cut, Mu, MuTilde, Statement, Variable},
+    };
+    use std::rc::Rc;
+
+    fn example_mutilde1() -> MuTilde {
+        MuTilde {
+            variable: "x".to_owned(),
+            statement: Rc::new(Statement::Done()),
+        }
+    }
+    fn example_mutilde2() -> MuTilde {
+        MuTilde {
+            variable: "x".to_owned(),
+            statement: Rc::new(
+                Cut {
+                    producer: Rc::new(
+                        Variable {
+                            var: "x".to_owned(),
+                        }
+                        .into(),
+                    ),
+                    consumer: Rc::new(
+                        Covariable {
+                            covar: "a".to_owned(),
+                        }
+                        .into(),
+                    ),
+                }
+                .into(),
+            ),
+        }
+    }
+
+    #[test]
+    fn transform_mutilde1() {
+        let result = example_mutilde1().transform(&mut Default::default());
+        let expected = example_mutilde1();
+        assert_eq!(result, expected)
+    }
+    #[test]
+    fn transform_mutilde2() {
+        let result = example_mutilde2().transform(&mut Default::default());
+        let expected = example_mutilde2();
+        assert_eq!(result, expected)
+    }
+    #[test]
+    fn bind_mutilde1() {
+        let result = example_mutilde1().bind(|_| |_| Statement::Done(), &mut Default::default());
+        let expected = Cut {
+            producer: Rc::new(
+                Mu {
+                    covariable: "a0".to_owned(),
+                    statement: Rc::new(Statement::Done()),
+                }
+                .into(),
+            ),
+            consumer: Rc::new(example_mutilde1().into()),
+        }
+        .into();
+        assert_eq!(result, expected)
+    }
+    #[test]
+    fn bind_mutilde2() {
+        let result = example_mutilde2().bind(|_| |_| Statement::Done(), &mut Default::default());
+        let expected = Cut {
+            producer: Rc::new(
+                Mu {
+                    covariable: "a0".to_owned(),
+                    statement: Rc::new(Statement::Done()),
+                }
+                .into(),
+            ),
+            consumer: Rc::new(example_mutilde2().into()),
+        }
+        .into();
+        assert_eq!(result, expected)
+    }
+}
