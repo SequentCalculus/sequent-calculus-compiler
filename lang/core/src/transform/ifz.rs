691c06941e028b695fdd5bb5a8ef94f1d2fe53a6
MarcoTz <marco.tzschentke@gmail.com>
2024-08-15 10:20:25 +0200

    added test for ifz

--- lang/core/src/transform/ifz.rs
+++ lang/core/src/transform/ifz.rs
@@ -24,3 +24,105 @@ impl NamingTransformation for IfZ {
         Rc::unwrap_or_clone(self.ifc).bind(cont, st)
     }
 }
+
+#[cfg(test)]
+mod transform_tests {
+    use crate::{
+        naming_transformation::NamingTransformation,
+        syntax::{Covariable, Cut, IfZ, Literal, MuTilde, Statement, Variable},
+    };
+    use std::rc::Rc;
+
+    fn example_ifz1() -> IfZ {
+        IfZ {
+            ifc: Rc::new(Literal { lit: 1 }.into()),
+            thenc: Rc::new(
+                Cut {
+                    producer: Rc::new(Literal { lit: 1 }.into()),
+                    consumer: Rc::new(
+                        Covariable {
+                            covar: "a".to_owned(),
+                        }
+                        .into(),
+                    ),
+                }
+                .into(),
+            ),
+            elsec: Rc::new(Statement::Done()),
+        }
+    }
+    fn example_ifz2() -> IfZ {
+        IfZ {
+            ifc: Rc::new(
+                Variable {
+                    var: "x".to_owned(),
+                }
+                .into(),
+            ),
+            thenc: Rc::new(Statement::Done()),
+            elsec: Rc::new(
+                Cut {
+                    producer: Rc::new(
+                        Variable {
+                            var: "x".to_owned(),
+                        }
+                        .into(),
+                    ),
+                    consumer: Rc::new(
+                        Covariable {
+                            covar: "a".to_owned(),
+                        }
+                        .into(),
+                    ),
+                }
+                .into(),
+            ),
+        }
+    }
+
+    #[test]
+    fn transform_ifz1() {
+        let result = example_ifz1().transform(&mut Default::default());
+        let expected = Cut {
+            producer: Rc::new(Literal { lit: 1 }.into()),
+            consumer: Rc::new(
+                MuTilde {
+                    variable: "x0".to_owned(),
+                    statement: Rc::new(
+                        IfZ {
+                            ifc: Rc::new(
+                                Variable {
+                                    var: "x0".to_owned(),
+                                }
+                                .into(),
+                            ),
+                            thenc: Rc::new(
+                                Cut {
+                                    producer: Rc::new(Literal { lit: 1 }.into()),
+                                    consumer: Rc::new(
+                                        Covariable {
+                                            covar: "a".to_owned(),
+                                        }
+                                        .into(),
+                                    ),
+                                }
+                                .into(),
+                            ),
+                            elsec: Rc::new(Statement::Done()),
+                        }
+                        .into(),
+                    ),
+                }
+                .into(),
+            ),
+        }
+        .into();
+        assert_eq!(result, expected)
+    }
+    #[test]
+    fn transform_ifz2() {
+        let result = example_ifz2().transform(&mut Default::default());
+        let expected = example_ifz2().into();
+        assert_eq!(result, expected)
+    }
+}
