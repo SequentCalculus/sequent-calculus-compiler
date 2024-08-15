6b1ad7d18dc3255fcfa7fa7ce59bbb4a768f46ea
mm0821 <mari.mueller@uni-tuebingen.de>
2024-08-15 15:00:49 +0200

    Do not lift continuation of Op in naming transformation

--- lang/core/src/transform/op.rs
+++ lang/core/src/transform/op.rs
@@ -1,32 +1,25 @@
 use super::super::{
     naming_transformation::{Bind, NamingTransformation, TransformState},
-    syntax::{Covar, Covariable, Op, Statement, Var, Variable},
+    syntax::{Op, Statement, Var, Variable},
 };
 use std::rc::Rc;
 
 impl NamingTransformation for Op {
     type Target = Statement;
-    ///N (⊙(p 1, p 2 ; c)) = bind(p 1) [λa1 .bind(p 2 ) [λa2 .bind(c) [λb. ⊙ (a1 , a 2; b)]]]
+    ///N(⊙(p1, p2; c)) = bind(p1)[λa1.bind(p2)[λa2.⊙ (a1, a2; c)]]
     fn transform(self, st: &mut TransformState) -> Statement {
         let cont = |a1: Var| {
             |st: &mut TransformState| {
                 Rc::unwrap_or_clone(self.snd).bind(
                     |a2: Var| {
-                        |st: &mut TransformState| {
-                            Rc::unwrap_or_clone(self.continuation).bind(
-                                |b: Covar| {
-                                    |_: &mut TransformState| {
-                                        Op {
-                                            fst: Rc::new(Variable { var: a1 }.into()),
-                                            op: self.op,
-                                            snd: Rc::new(Variable { var: a2 }.into()),
-                                            continuation: Rc::new(Covariable { covar: b }.into()),
-                                        }
-                                        .into()
-                                    }
-                                },
-                                st,
-                            )
+                        |_: &mut TransformState| {
+                            Op {
+                                fst: Rc::new(Variable { var: a1 }.into()),
+                                op: self.op,
+                                snd: Rc::new(Variable { var: a2 }.into()),
+                                continuation: self.continuation,
+                            }
+                            .into()
                         }
                     },
                     st,
