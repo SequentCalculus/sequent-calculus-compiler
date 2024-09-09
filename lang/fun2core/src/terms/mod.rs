86d03ca29b2095259a011c5ad6a17579a14e2ce6
MarcoTz <marco.tzschentke@gmail.com>
2024-09-09 19:33:51 +0200

    added contexts to xcases and toplevel definitions

--- lang/fun2core/src/terms/mod.rs
+++ lang/fun2core/src/terms/mod.rs
@@ -70,3 +70,293 @@ impl CompileWithCont for fun::syntax::terms::Term {
         }
     }
 }
+
+#[cfg(test)]
+mod compile_tests {
+
+    use crate::definition::CompileWithCont;
+    use fun::syntax::terms::{
+        Case, Clause, Cocase, Constructor, Destructor, Fun, Goto, IfZ, Label, Let, Op, Paren, Term,
+    };
+    use fun::syntax::{BinOp, Ctor, Dtor};
+    use std::rc::Rc;
+
+    fn example_var() -> Term {
+        Term::Var("x".to_owned())
+    }
+
+    fn example_lit() -> Term {
+        Term::Lit(1)
+    }
+
+    fn example_op() -> Op {
+        Op {
+            fst: Rc::new(Term::Lit(1)),
+            op: BinOp::Sum,
+            snd: Rc::new(Term::Lit(2)),
+        }
+    }
+
+    fn example_ifz() -> IfZ {
+        IfZ {
+            ifc: Rc::new(Term::Lit(0)),
+            thenc: Rc::new(Term::Var("x".to_owned())),
+            elsec: Rc::new(Term::Var("y".to_owned())),
+        }
+    }
+
+    fn example_let() -> Let {
+        Let {
+            variable: "x".to_owned(),
+            bound_term: Rc::new(Term::Lit(1)),
+            in_term: Rc::new(Term::Var("x".to_owned())),
+        }
+    }
+
+    fn example_fun() -> Fun {
+        Fun {
+            name: "mult".to_owned(),
+            args: vec![Term::Var("x".to_owned())],
+            coargs: vec![],
+        }
+    }
+
+    fn example_ctor() -> Constructor {
+        Constructor {
+            id: Ctor::Nil,
+            args: vec![],
+        }
+    }
+
+    fn example_dtor() -> Destructor {
+        Destructor {
+            id: Dtor::Hd,
+            args: vec![],
+            destructee: Rc::new(Term::Var("x".to_owned())),
+        }
+    }
+
+    fn example_case() -> Case {
+        Case {
+            cases: vec![Clause {
+                xtor: Ctor::Nil,
+                context: vec![],
+                rhs: Term::Lit(1),
+            }],
+            destructee: Rc::new(Term::Var("x".to_owned())),
+        }
+    }
+
+    fn example_cocase() -> Cocase {
+        Cocase {
+            cocases: vec![
+                Clause {
+                    xtor: Dtor::Fst,
+                    context: vec![],
+                    rhs: Term::Lit(1),
+                },
+                Clause {
+                    xtor: Dtor::Snd,
+                    context: vec![],
+                    rhs: Term::Lit(2),
+                },
+            ],
+        }
+    }
+
+    fn example_goto() -> Goto {
+        Goto {
+            term: Rc::new(Term::Lit(1)),
+            target: "a".to_owned(),
+        }
+    }
+
+    fn example_label() -> Label {
+        Label {
+            label: "a".to_owned(),
+            term: Rc::new(Term::Lit(1)),
+        }
+    }
+    fn example_paren() -> Paren {
+        Paren {
+            inner: Rc::new(Term::Lit(1)),
+        }
+    }
+
+    #[test]
+    fn compile_var() {
+        let result = example_var().compile_opt(&mut Default::default());
+        let expected = core::syntax::Variable {
+            var: "x".to_owned(),
+        }
+        .into();
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_lit() {
+        let result = example_lit().compile_opt(&mut Default::default());
+        let expected = core::syntax::Literal { lit: 1 }.into();
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_op() {
+        let result = <Op as Into<Term>>::into(example_op()).compile_opt(&mut Default::default());
+        let expected = example_op().compile_opt(&mut Default::default());
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_ifz() {
+        let result = <IfZ as Into<Term>>::into(example_ifz()).compile_opt(&mut Default::default());
+        let expected = example_ifz().compile_opt(&mut Default::default());
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_let() {
+        let result = <Let as Into<Term>>::into(example_let()).compile_opt(&mut Default::default());
+        let expected = example_let().compile_opt(&mut Default::default());
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_inner_let() {
+        let result = <Let as Into<Term>>::into(example_let()).compile_with_cont(
+            core::syntax::Covariable {
+                covar: "a".to_owned(),
+            }
+            .into(),
+            &mut Default::default(),
+        );
+        let expected = example_let().compile_with_cont(
+            core::syntax::Covariable {
+                covar: "a".to_owned(),
+            }
+            .into(),
+            &mut Default::default(),
+        );
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_fun() {
+        let result = <Fun as Into<Term>>::into(example_fun()).compile_opt(&mut Default::default());
+        let expected = example_fun().compile_opt(&mut Default::default());
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_ctor() {
+        let result =
+            <Constructor as Into<Term>>::into(example_ctor()).compile_opt(&mut Default::default());
+        let expected = example_ctor().compile_opt(&mut Default::default());
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_dtor() {
+        let result =
+            <Destructor as Into<Term>>::into(example_dtor()).compile_opt(&mut Default::default());
+        let expected = example_dtor().compile_opt(&mut Default::default());
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_inner_dtor() {
+        let result = <Destructor as Into<Term>>::into(example_dtor()).compile_with_cont(
+            core::syntax::Covariable {
+                covar: "a".to_owned(),
+            }
+            .into(),
+            &mut Default::default(),
+        );
+        let expected = example_dtor().compile_with_cont(
+            core::syntax::Covariable {
+                covar: "a".to_owned(),
+            }
+            .into(),
+            &mut Default::default(),
+        );
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_case() {
+        let result =
+            <Case as Into<Term>>::into(example_case()).compile_opt(&mut Default::default());
+        let expected = example_case().compile_opt(&mut Default::default());
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_cocase() {
+        let result =
+            <Cocase as Into<Term>>::into(example_cocase()).compile_opt(&mut Default::default());
+        let expected = example_cocase().compile_opt(&mut Default::default());
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_goto() {
+        let result =
+            <Goto as Into<Term>>::into(example_goto()).compile_opt(&mut Default::default());
+        let expected = example_goto().compile_opt(&mut Default::default());
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_label() {
+        let result =
+            <Label as Into<Term>>::into(example_label()).compile_opt(&mut Default::default());
+        let expected = example_label().compile_opt(&mut Default::default());
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_inner_label() {
+        let result = <Label as Into<Term>>::into(example_label()).compile_with_cont(
+            core::syntax::Covariable {
+                covar: "a".to_owned(),
+            }
+            .into(),
+            &mut Default::default(),
+        );
+        let expected = example_label().compile_with_cont(
+            core::syntax::Covariable {
+                covar: "a".to_owned(),
+            }
+            .into(),
+            &mut Default::default(),
+        );
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_paren() {
+        let result =
+            <Paren as Into<Term>>::into(example_paren()).compile_opt(&mut Default::default());
+        let expected = example_paren().compile_opt(&mut Default::default());
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn compile_inner_paren() {
+        let result = <Paren as Into<Term>>::into(example_paren()).compile_with_cont(
+            core::syntax::Covariable {
+                covar: "a".to_owned(),
+            }
+            .into(),
+            &mut Default::default(),
+        );
+        let expected = example_paren().compile_with_cont(
+            core::syntax::Covariable {
+                covar: "a".to_owned(),
+            }
+            .into(),
+            &mut Default::default(),
+        );
+        assert_eq!(result, expected)
+    }
+}
