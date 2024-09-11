cdddfa94bff033011a802ada1a61f205aa774742
MarcoTz <marco.tzschentke@gmail.com>
2024-09-11 15:35:18 +0200

    added display and tests

--- lang/core/src/syntax/context.rs
+++ lang/core/src/syntax/context.rs
@@ -1,8 +1,49 @@
-use super::{types::Ty, Covariable, Variable};
+use super::{types::Ty, Covar, Var};
+use std::fmt;
 
 pub enum ContextBinding {
-    VarBinding { var: Variable, ty: Ty },
-    CovarBinding { covar: Covariable, ty: Ty },
+    VarBinding { var: Var, ty: Ty },
+    CovarBinding { covar: Covar, ty: Ty },
 }
 
 pub type TypingContext = Vec<ContextBinding>;
+
+impl fmt::Display for ContextBinding {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ContextBinding::VarBinding { var, ty } => write!(f, "{var} : {ty}"),
+            ContextBinding::CovarBinding { covar, ty } => write!(f, "'{covar} :cnt {ty}"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod context_tests {
+    use super::{ContextBinding, Ty};
+
+    #[test]
+    fn display_var() {
+        let result = format!(
+            "{}",
+            ContextBinding::VarBinding {
+                var: "x".to_owned(),
+                ty: Ty::Int()
+            }
+        );
+        let expected = "x : Int";
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn display_covar() {
+        let result = format!(
+            "{}",
+            ContextBinding::CovarBinding {
+                covar: "a".to_owned(),
+                ty: Ty::Int()
+            }
+        );
+        let expected = "'a :cnt Int";
+        assert_eq!(result, expected)
+    }
+}
--- lang/core/src/syntax/types.rs
+++ lang/core/src/syntax/types.rs
@@ -1,6 +1,35 @@
 use super::Name;
+use std::fmt;
 
 pub enum Ty {
     Int(),
     Decl(Name),
+}
+
+impl fmt::Display for Ty {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Ty::Int() => f.write_str("Int"),
+            Ty::Decl(name) => f.write_str(name),
+        }
+    }
+}
+
+#[cfg(test)]
+mod ty_tests {
+    use super::Ty;
+
+    #[test]
+    fn display_int() {
+        let result = format!("{}", Ty::Int());
+        let expected = "Int";
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn display_list() {
+        let result = format!("{}", Ty::Decl("Listint".to_owned()));
+        let expected = "Listint";
+        assert_eq!(result, expected)
+    }
 }
