f054f9776585943a1613f4b4ad046dc888b7bacc
Marco <38535746+MarcoTz@users.noreply.github.com>
2024-10-09 15:47:33 +0200

    Adds test to terms (#23)
    * added tests for xvar
    
    * removed consumer covar testst
    
    * added literal tests
    
    * added mu tests
    
    * added xtor tests
    
    * added xcase tests

--- lang/core/src/syntax/term/mu.rs
+++ lang/core/src/syntax/term/mu.rs
@@ -19,11 +19,11 @@ pub struct Mu<T: PrdCns> {
 impl<T: PrdCns> std::fmt::Display for Mu<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let prefix = if self.prdcns.is_prd() {
-            "mu"
+            format!("mu '{}", self.variable)
         } else {
-            "mutilde"
+            format!("mutilde {}", self.variable)
         };
-        write!(f, "{} {}. {}", prefix, self.variable, self.statement)
+        write!(f, "{}. {}", prefix, self.statement)
     }
 }
 
@@ -48,5 +48,108 @@ impl<T: PrdCns> FreeV for Mu<T> {
 impl<T: PrdCns> From<Mu<T>> for Term<T> {
     fn from(value: Mu<T>) -> Self {
         Term::Mu(value)
+    }
+}
+
+#[cfg(test)]
+mod mu_tests {
+    use super::{FreeV, Mu};
+    use crate::syntax::{
+        statement::Cut,
+        term::{Cns, Prd},
+        Covariable, Variable,
+    };
+    use std::{collections::HashSet, rc::Rc};
+
+    fn example_mu() -> Mu<Prd> {
+        Mu {
+            prdcns: Prd,
+            variable: "a".to_owned(),
+            statement: Rc::new(
+                Cut {
+                    producer: Rc::new(
+                        Variable {
+                            var: "x".to_owned(),
+                        }
+                        .into(),
+                    ),
+                    consumer: Rc::new(
+                        Covariable {
+                            covar: "a".to_owned(),
+                        }
+                        .into(),
+                    ),
+                }
+                .into(),
+            ),
+        }
+        .into()
+    }
+
+    fn example_mu_tilde() -> Mu<Cns> {
+        Mu {
+            prdcns: Cns,
+            variable: "x".to_owned(),
+            statement: Rc::new(
+                Cut {
+                    producer: Rc::new(
+                        Variable {
+                            var: "x".to_owned(),
+                        }
+                        .into(),
+                    ),
+                    consumer: Rc::new(
+                        Covariable {
+                            covar: "a".to_owned(),
+                        }
+                        .into(),
+                    ),
+                }
+                .into(),
+            ),
+        }
+        .into()
+    }
+
+    #[test]
+    fn display_mu() {
+        let result = format!("{}", example_mu());
+        let expected = "mu 'a. <x | 'a>".to_owned();
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn display_mu_tilde() {
+        let result = format!("{}", example_mu_tilde());
+        let expected = "mutilde x. <x | 'a>".to_owned();
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn free_vars_mu() {
+        let result = example_mu().free_vars();
+        let expected = HashSet::from(["x".to_owned()]);
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn free_vars_mu_tilde() {
+        let result = example_mu_tilde().free_vars();
+        let expected = HashSet::new();
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn free_covars_mu() {
+        let result = example_mu().free_covars();
+        let expected = HashSet::new();
+        assert_eq!(result, expected)
+    }
+
+    #[test]
+    fn free_covars_mu_tilde() {
+        let result = example_mu_tilde().free_covars();
+        let expected = HashSet::from(["a".to_owned()]);
+        assert_eq!(result, expected)
     }
 }
