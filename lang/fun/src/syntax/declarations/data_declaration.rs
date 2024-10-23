4a1e89701ed1f7b8b4956c24fcdb9dabd9dad0d5
David Binder <david.binder@uni-tuebingen.de>
2024-10-23 15:08:32 -0700

    Split declarations

---
+++ lang/fun/src/syntax/declarations/data_declaration.rs
@@ -0,0 +1,125 @@
+use codespan::Span;
+use derivative::Derivative;
+use printer::{
+    theme::ThemeExt,
+    tokens::{COMMA, DATA},
+    util::BracesExt,
+    DocAllocator, Print,
+};
+
+use crate::syntax::{context::TypingContext, empty_braces, Name};
+
+use super::Declaration;
+
+#[derive(Derivative, Clone, Debug)]
+#[derivative(PartialEq, Eq)]
+pub struct CtorSig {
+    #[derivative(PartialEq = "ignore")]
+    pub span: Span,
+    pub name: Name,
+    pub args: TypingContext,
+}
+
+#[derive(Derivative, Clone, Debug)]
+#[derivative(PartialEq, Eq)]
+pub struct DataDeclaration {
+    #[derivative(PartialEq = "ignore")]
+    pub span: Span,
+    pub name: Name,
+    pub ctors: Vec<CtorSig>,
+}
+
+impl From<DataDeclaration> for Declaration {
+    fn from(data: DataDeclaration) -> Declaration {
+        Declaration::DataDeclaration(data)
+    }
+}
+
+impl Print for DataDeclaration {
+    fn print<'a>(
+        &'a self,
+        cfg: &printer::PrintCfg,
+        alloc: &'a printer::Alloc<'a>,
+    ) -> printer::Builder<'a> {
+        let head = alloc
+            .keyword(DATA)
+            .append(alloc.space())
+            .append(alloc.typ(&self.name))
+            .append(alloc.space());
+
+        let sep = alloc.text(COMMA).append(alloc.line());
+
+        let body = if self.ctors.is_empty() {
+            empty_braces(alloc)
+        } else {
+            alloc
+                .line()
+                .append(
+                    alloc.intersperse(self.ctors.iter().map(|ctor| ctor.print(cfg, alloc)), sep),
+                )
+                .nest(cfg.indent)
+                .append(alloc.line())
+                .braces_anno()
+        };
+
+        head.append(body.group())
+    }
+}
+
+impl Print for CtorSig {
+    fn print<'a>(
+        &'a self,
+        cfg: &printer::PrintCfg,
+        alloc: &'a printer::Alloc<'a>,
+    ) -> printer::Builder<'a> {
+        alloc
+            .ctor(&self.name)
+            .append(self.args.print(cfg, alloc).parens())
+    }
+}
+
+#[cfg(test)]
+mod data_declaration_tests {
+    use codespan::Span;
+    use printer::Print;
+
+    use crate::syntax::{context::ContextBinding, types::Ty};
+
+    use super::{CtorSig, DataDeclaration};
+
+    /// Lists containing Int
+    fn example_list() -> DataDeclaration {
+        let nil = CtorSig {
+            span: Span::default(),
+            name: "Nil".to_owned(),
+            args: vec![],
+        };
+        let cons = CtorSig {
+            span: Span::default(),
+            name: "Cons".to_owned(),
+            args: vec![
+                ContextBinding::TypedVar {
+                    var: "x".to_owned(),
+                    ty: Ty::mk_int(),
+                },
+                ContextBinding::TypedVar {
+                    var: "xs".to_owned(),
+                    ty: Ty::mk_decl("ListInt"),
+                },
+            ],
+        };
+
+        DataDeclaration {
+            span: Span::default(),
+            name: "ListInt".to_owned(),
+            ctors: vec![nil, cons],
+        }
+    }
+
+    #[test]
+    fn display_list() {
+        let result = example_list().print_to_string(Default::default());
+        let expected = "data ListInt { Nil(), Cons(x : Int, xs : ListInt) }";
+        assert_eq!(result, expected)
+    }
+}
---
+++ lang/fun/src/syntax/declarations/definition.rs
@@ -0,0 +1,102 @@
+use codespan::Span;
+use derivative::Derivative;
+use printer::{
+    theme::ThemeExt,
+    tokens::{COLON, COLONEQ, DEF, SEMI},
+    DocAllocator, Print,
+};
+
+use crate::syntax::{context::TypingContext, terms::Term, types::Ty, Name};
+
+use super::Declaration;
+
+/// A toplevel function definition in a module.
+#[derive(Derivative, Debug, Clone)]
+#[derivative(PartialEq, Eq)]
+pub struct Definition {
+    #[derivative(PartialEq = "ignore")]
+    pub span: Span,
+    pub name: Name,
+    pub context: TypingContext,
+    pub body: Term,
+    pub ret_ty: Ty,
+}
+
+impl Print for Definition {
+    fn print<'a>(
+        &'a self,
+        cfg: &printer::PrintCfg,
+        alloc: &'a printer::Alloc<'a>,
+    ) -> printer::Builder<'a> {
+        let head = alloc
+            .keyword(DEF)
+            .append(alloc.space())
+            .append(self.name.clone())
+            .append(self.context.print(cfg, alloc).parens())
+            .append(alloc.space())
+            .append(COLON)
+            .append(alloc.space())
+            .append(self.ret_ty.print(cfg, alloc))
+            .append(alloc.space())
+            .append(COLONEQ);
+
+        let body = alloc
+            .line()
+            .append(self.body.print(cfg, alloc))
+            .append(SEMI)
+            .nest(cfg.indent);
+
+        head.append(body).group()
+    }
+}
+
+impl From<Definition> for Declaration {
+    fn from(value: Definition) -> Self {
+        Declaration::Definition(value)
+    }
+}
+
+#[cfg(test)]
+mod definition_tests {
+    use codespan::Span;
+    use printer::Print;
+
+    use crate::{
+        parser::fun,
+        syntax::{
+            declarations::Module,
+            terms::{Lit, Term},
+            types::Ty,
+        },
+    };
+
+    use super::Definition;
+
+    /// A definition with no arguments:
+    fn simple_definition() -> Definition {
+        Definition {
+            span: Span::default(),
+            name: "x".to_string(),
+            context: vec![],
+            body: Term::Lit(Lit::mk(4)),
+            ret_ty: Ty::mk_int(),
+        }
+    }
+
+    #[test]
+    fn display_simple() {
+        assert_eq!(
+            simple_definition().print_to_string(Default::default()),
+            "def x() : Int := 4;".to_string()
+        )
+    }
+
+    #[test]
+    fn parse_simple() {
+        let parser = fun::ProgParser::new();
+        let module = Module {
+            declarations: vec![simple_definition().into()],
+        };
+        assert_eq!(parser.parse("def x() : Int := 4;"), Ok(module));
+    }
+}
