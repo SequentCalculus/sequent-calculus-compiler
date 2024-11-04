05d5c8ca4c4b021219f7afb5bd8661ee46592f6c
MarcoTz <marco.tzschentke@gmail.com>
2024-11-04 15:37:22 +0100

    added tests for contetxt:

--- lang/fun/src/typing/check/context.rs
+++ lang/fun/src/typing/check/context.rs
@@ -55,3 +55,80 @@ pub fn compare_typing_contexts(
     }
     Ok(())
 }
+
+#[cfg(test)]
+mod context_tests {
+    use super::{check_typing_context, compare_typing_contexts};
+    use crate::{
+        parser::util::ToMiette,
+        syntax::{context::ContextBinding, types::Ty},
+        typing::symbol_table::{Polarity, SymbolTable},
+    };
+    use codespan::Span;
+
+    #[test]
+    fn context_check() {
+        let mut symbol_table = SymbolTable::default();
+        symbol_table
+            .ty_ctors
+            .insert("FunIntInt".to_owned(), (Polarity::Codata, vec![]));
+        let result = check_typing_context(
+            &vec![
+                ContextBinding::TypedVar {
+                    var: "x".to_owned(),
+                    ty: Ty::mk_int(),
+                },
+                ContextBinding::TypedCovar {
+                    covar: "a".to_owned(),
+                    ty: Ty::mk_decl("FunIntInt"),
+                },
+            ],
+            &symbol_table,
+        );
+        assert!(result.is_ok())
+    }
+
+    #[test]
+    fn context_check_fail() {
+        let result = check_typing_context(
+            &vec![ContextBinding::TypedVar {
+                var: "x".to_owned(),
+                ty: Ty::mk_decl("ListInt"),
+            }],
+            &SymbolTable::default(),
+        );
+        assert!(result.is_err())
+    }
+
+    #[test]
+    fn context_compare() {
+        let result = compare_typing_contexts(
+            &Span::default().to_miette(),
+            &vec![ContextBinding::TypedVar {
+                var: "x".to_owned(),
+                ty: Ty::mk_int(),
+            }],
+            &vec![ContextBinding::TypedVar {
+                var: "y".to_owned(),
+                ty: Ty::mk_int(),
+            }],
+        );
+        assert!(result.is_ok())
+    }
+
+    #[test]
+    fn context_compare_fail() {
+        let result = compare_typing_contexts(
+            &Span::default().to_miette(),
+            &vec![ContextBinding::TypedVar {
+                var: "x".to_owned(),
+                ty: Ty::mk_int(),
+            }],
+            &vec![ContextBinding::TypedCovar {
+                covar: "a".to_owned(),
+                ty: Ty::mk_int(),
+            }],
+        );
+        assert!(result.is_err())
+    }
+}
