633d33dc8f4740ba1ff7366656af3125a40e6055
David Binder <david.binder@uni-tuebingen.de>
2024-11-05 16:16:23 +0100

    Factor out constructor

--- lang/fun/src/syntax/terms/mod.rs
+++ lang/fun/src/syntax/terms/mod.rs
@@ -11,8 +11,10 @@ use printer::{
     DocAllocator, Print,
 };
 
+mod constructor;
 mod lit;
 mod op;
+pub use constructor::*;
 pub use lit::*;
 pub use op::*;
 
@@ -298,91 +300,6 @@ mod fun_tests {
     fn parse_extended() {
         let parser = fun::TermParser::new();
         assert_eq!(parser.parse("foo(2, 'a)"), Ok(example_extended().into()));
-    }
-}
-
-// Constructor
-//
-//
-
-#[derive(Derivative, Debug, Clone)]
-#[derivative(PartialEq, Eq)]
-pub struct Constructor {
-    #[derivative(PartialEq = "ignore")]
-    pub span: Span,
-    pub id: Name,
-    pub args: Substitution,
-}
-
-impl Print for Constructor {
-    fn print<'a>(
-        &'a self,
-        cfg: &printer::PrintCfg,
-        alloc: &'a printer::Alloc<'a>,
-    ) -> printer::Builder<'a> {
-        if self.args.is_empty() {
-            alloc.ctor(&self.id)
-        } else {
-            alloc
-                .ctor(&self.id)
-                .append(self.args.print(cfg, alloc).parens())
-        }
-    }
-}
-
-impl From<Constructor> for Term {
-    fn from(value: Constructor) -> Self {
-        Term::Constructor(value)
-    }
-}
-
-#[cfg(test)]
-mod constructor_tests {
-    use codespan::Span;
-    use printer::Print;
-
-    use super::{Constructor, Lit, Term};
-    use crate::parser::fun;
-
-    fn example_nil() -> Constructor {
-        Constructor {
-            span: Span::default(),
-            id: "Nil".to_owned(),
-            args: vec![],
-        }
-    }
-
-    fn example_tup() -> Constructor {
-        Constructor {
-            span: Span::default(),
-            id: "Tup".to_owned(),
-            args: vec![Term::Lit(Lit::mk(2)).into(), Term::Lit(Lit::mk(4)).into()],
-        }
-    }
-
-    #[test]
-    fn display_nil() {
-        assert_eq!(example_nil().print_to_string(Default::default()), "Nil")
-    }
-
-    #[test]
-    fn parse_nil() {
-        let parser = fun::TermParser::new();
-        assert_eq!(parser.parse("Nil"), Ok(example_nil().into()));
-    }
-
-    #[test]
-    fn display_tup() {
-        assert_eq!(
-            example_tup().print_to_string(Default::default()),
-            "Tup(2, 4)"
-        )
-    }
-
-    #[test]
-    fn parse_tup() {
-        let parser = fun::TermParser::new();
-        assert_eq!(parser.parse("Tup(2,4)"), Ok(example_tup().into()));
     }
 }
 
