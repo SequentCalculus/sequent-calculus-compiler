ac86dfcf4825ee05bd3ec60581fa840bb51c01bc
David Binder <david.binder@uni-tuebingen.de>
2024-11-05 16:34:32 +0100

    Factor out IfZ

--- lang/fun/src/syntax/terms/mod.rs
+++ lang/fun/src/syntax/terms/mod.rs
@@ -4,13 +4,14 @@ use codespan::Span;
 use derivative::Derivative;
 use printer::{
     theme::ThemeExt,
-    tokens::{CASE, COCASE, COLON, COMMA, DOT, EQ, FAT_ARROW, IFZ, IN, LET},
+    tokens::{CASE, COCASE, COLON, DOT, EQ, FAT_ARROW, IN, LET},
     DocAllocator, Print,
 };
 
 mod constructor;
 mod destructor;
 mod goto;
+mod ifz;
 mod label;
 mod lit;
 mod op;
@@ -18,6 +19,7 @@ mod paren;
 pub use constructor::*;
 pub use destructor::*;
 pub use goto::*;
+pub use ifz::*;
 pub use label::*;
 pub use lit::*;
 pub use op::*;
@@ -62,80 +64,6 @@ impl<T: Print> Print for Clause<T> {
                 .append(alloc.space())
                 .append(self.rhs.print(cfg, alloc))
         }
-    }
-}
-
-// IfZ
-//
-//
-
-#[derive(Derivative, Debug, Clone)]
-#[derivative(PartialEq, Eq)]
-pub struct IfZ {
-    #[derivative(PartialEq = "ignore")]
-    pub span: Span,
-    pub ifc: Rc<Term>,
-    pub thenc: Rc<Term>,
-    pub elsec: Rc<Term>,
-}
-
-impl Print for IfZ {
-    fn print<'a>(
-        &'a self,
-        cfg: &printer::PrintCfg,
-        alloc: &'a printer::Alloc<'a>,
-    ) -> printer::Builder<'a> {
-        alloc.keyword(IFZ).append(
-            self.ifc
-                .print(cfg, alloc)
-                .append(COMMA)
-                .append(alloc.space())
-                .append(self.thenc.print(cfg, alloc))
-                .append(COMMA)
-                .append(alloc.space())
-                .append(self.elsec.print(cfg, alloc))
-                .parens(),
-        )
-    }
-}
-
-impl From<IfZ> for Term {
-    fn from(value: IfZ) -> Self {
-        Term::IfZ(value)
-    }
-}
-
-#[cfg(test)]
-mod ifz_tests {
-    use codespan::Span;
-    use printer::Print;
-
-    use crate::parser::fun;
-    use std::rc::Rc;
-
-    use super::{IfZ, Lit, Term};
-
-    fn example() -> IfZ {
-        IfZ {
-            span: Span::default(),
-            ifc: Rc::new(Term::Lit(Lit::mk(0))),
-            thenc: Rc::new(Term::Lit(Lit::mk(2))),
-            elsec: Rc::new(Term::Lit(Lit::mk(4))),
-        }
-    }
-
-    #[test]
-    fn display() {
-        assert_eq!(
-            example().print_to_string(Default::default()),
-            "ifz(0, 2, 4)"
-        )
-    }
-
-    #[test]
-    fn parse() {
-        let parser = fun::TermParser::new();
-        assert_eq!(parser.parse("ifz(0, 2, 4)"), Ok(example().into()));
     }
 }
 
