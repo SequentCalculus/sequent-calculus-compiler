19d66191e4c66b6462057f4d7249222e329bdc7a
David Binder <david.binder@uni-tuebingen.de>
2024-11-05 16:09:45 +0100

    Factor out Op and BinOp

--- lang/fun/src/syntax/terms/mod.rs
+++ lang/fun/src/syntax/terms/mod.rs
@@ -11,7 +11,10 @@ use printer::{
     DocAllocator, Print,
 };
 
-use super::{context::TypingContext, print_cases, types::Ty, BinOp, Covariable, Name, Variable};
+mod op;
+pub use op::*;
+
+use super::{context::TypingContext, print_cases, types::Ty, Covariable, Name, Variable};
 use crate::syntax::substitution::Substitution;
 
 // Clause
@@ -50,151 +53,6 @@ impl<T: Print> Print for Clause<T> {
                 .append(alloc.space())
                 .append(self.rhs.print(cfg, alloc))
         }
-    }
-}
-
-// Op
-//
-//
-
-#[derive(Derivative, Debug, Clone)]
-#[derivative(PartialEq, Eq)]
-pub struct Op {
-    #[derivative(PartialEq = "ignore")]
-    pub span: Span,
-    pub fst: Rc<Term>,
-    pub op: BinOp,
-    pub snd: Rc<Term>,
-}
-
-impl Print for Op {
-    fn print<'a>(
-        &'a self,
-        cfg: &printer::PrintCfg,
-        alloc: &'a printer::Alloc<'a>,
-    ) -> printer::Builder<'a> {
-        self.fst
-            .print(cfg, alloc)
-            .append(alloc.space())
-            .append(self.op.print(cfg, alloc))
-            .append(alloc.space())
-            .append(self.snd.print(cfg, alloc))
-    }
-}
-
-impl From<Op> for Term {
-    fn from(value: Op) -> Self {
-        Term::Op(value)
-    }
-}
-
-#[cfg(test)]
-mod op_tests {
-    use std::rc::Rc;
-
-    use codespan::Span;
-    use printer::Print;
-
-    use crate::parser::fun;
-
-    use super::{BinOp, Lit, Op, Paren, Term};
-
-    fn example_prod() -> Op {
-        Op {
-            span: Span::default(),
-            fst: Rc::new(Term::Lit(Lit::mk(2))),
-            op: BinOp::Prod,
-            snd: Rc::new(Term::Lit(Lit::mk(4))),
-        }
-    }
-
-    #[test]
-    fn display_prod() {
-        assert_eq!(example_prod().print_to_string(Default::default()), "2 * 4")
-    }
-
-    #[test]
-    fn parse_prod() {
-        let parser = fun::TermParser::new();
-        assert_eq!(parser.parse("2 * 4"), Ok(example_prod().into()));
-    }
-
-    fn example_sum() -> Op {
-        Op {
-            span: Span::default(),
-            fst: Rc::new(Term::Lit(Lit::mk(2))),
-            op: BinOp::Sum,
-            snd: Rc::new(Term::Lit(Lit::mk(4))),
-        }
-    }
-
-    #[test]
-    fn display_sum() {
-        assert_eq!(example_sum().print_to_string(Default::default()), "2 + 4")
-    }
-
-    #[test]
-    fn parse_sum() {
-        let parser = fun::TermParser::new();
-        assert_eq!(parser.parse("2 + 4"), Ok(example_sum().into()));
-    }
-
-    fn example_sub() -> Op {
-        Op {
-            span: Span::default(),
-            fst: Rc::new(Term::Lit(Lit::mk(2))),
-            op: BinOp::Sub,
-            snd: Rc::new(Term::Lit(Lit::mk(4))),
-        }
-    }
-
-    #[test]
-    fn display_sub() {
-        assert_eq!(example_sub().print_to_string(Default::default()), "2 - 4")
-    }
-
-    #[test]
-    fn parse_sub() {
-        let parser = fun::TermParser::new();
-        assert_eq!(parser.parse("2 - 4"), Ok(example_sub().into()));
-    }
-
-    /// (2 * 3) * 4
-    fn example_parens() -> Op {
-        Op {
-            span: Span::default(),
-            fst: Rc::new(
-                Paren {
-                    span: Span::default(),
-                    inner: Rc::new(
-                        Op {
-                            span: Span::default(),
-                            fst: Rc::new(Term::Lit(Lit::mk(2))),
-                            op: BinOp::Prod,
-                            snd: Rc::new(Term::Lit(Lit::mk(3))),
-                        }
-                        .into(),
-                    ),
-                }
-                .into(),
-            ),
-            op: BinOp::Prod,
-            snd: Rc::new(Term::Lit(Lit::mk(4))),
-        }
-    }
-
-    #[test]
-    fn display_parens() {
-        assert_eq!(
-            example_parens().print_to_string(Default::default()),
-            "(2 * 3) * 4"
-        )
-    }
-
-    #[test]
-    fn parse_parens() {
-        let parser = fun::TermParser::new();
-        assert_eq!(parser.parse("(2 * 3) * 4"), Ok(example_parens().into()));
     }
 }
 
