2586762afc026ed6308beac2937796ed4952dbb3
MarcoTz <marco.tzschentke@gmail.com>
2024-11-11 11:16:09 +0100

    factored out checking declarations

---
+++ lang/fun/src/typing/check/declarations.rs
@@ -0,0 +1,56 @@
+use super::{check_type, check_typing_context, terms::Check};
+use crate::{
+    syntax::declarations::{
+        CodataDeclaration, CtorSig, DataDeclaration, Declaration, Definition, DtorSig,
+    },
+    typing::{errors::Error, symbol_table::SymbolTable},
+};
+
+// Checking toplevel declarations
+//
+//
+
+pub fn check_declaration(decl: &Declaration, symbol_table: &SymbolTable) -> Result<(), Error> {
+    match decl {
+        Declaration::Definition(definition) => check_definition(definition, symbol_table),
+        Declaration::DataDeclaration(data_declaration) => {
+            check_data_declaration(data_declaration, symbol_table)
+        }
+        Declaration::CodataDeclaration(codata_declaration) => {
+            check_codata_declaration(codata_declaration, symbol_table)
+        }
+    }
+}
+
+fn check_definition(def: &Definition, symbol_table: &SymbolTable) -> Result<(), Error> {
+    check_typing_context(&def.context, symbol_table)?;
+    check_type(&def.ret_ty, symbol_table)?;
+    def.body.check(symbol_table, &def.context, &def.ret_ty)
+}
+
+fn check_data_declaration(decl: &DataDeclaration, symbol_table: &SymbolTable) -> Result<(), Error> {
+    for ctor in decl.ctors.iter() {
+        check_ctor_sig(ctor, symbol_table)?;
+    }
+    Ok(())
+}
+fn check_codata_declaration(
+    decl: &CodataDeclaration,
+    symbol_table: &SymbolTable,
+) -> Result<(), Error> {
+    for dtor in decl.dtors.iter() {
+        check_dtor_sig(dtor, symbol_table)?;
+    }
+    Ok(())
+}
+
+fn check_ctor_sig(ctor: &CtorSig, symbol_table: &SymbolTable) -> Result<(), Error> {
+    check_typing_context(&ctor.args, symbol_table)?;
+    Ok(())
+}
+
+fn check_dtor_sig(dtor: &DtorSig, symbol_table: &SymbolTable) -> Result<(), Error> {
+    check_typing_context(&dtor.args, symbol_table)?;
+    check_type(&dtor.cont_ty, symbol_table)?;
+    Ok(())
+}
--- lang/fun/src/typing/check/mod.rs
+++ lang/fun/src/typing/check/mod.rs
@@ -7,6 +7,7 @@ use printer::Print;
 pub mod case;
 pub mod cocase;
 pub mod ctor;
+pub mod declarations;
 pub mod dtor;
 pub mod fun;
 pub mod goto;
@@ -19,15 +20,14 @@ pub mod paren;
 pub mod terms;
 pub mod var;
 
+use declarations::check_declaration;
 use terms::Check;
 
 use crate::{
     parser::util::ToMiette,
     syntax::{
         context::{ContextBinding, TypingContext},
-        declarations::{
-            CodataDeclaration, CtorSig, DataDeclaration, Declaration, Definition, DtorSig, Module,
-        },
+        declarations::Module,
         substitution::{Substitution, SubstitutionBinding},
         types::Ty,
         Covariable, Name, Variable,
@@ -166,56 +166,6 @@ fn check_typing_context(ctx: &TypingContext, symbol_table: &SymbolTable) -> Resu
             ContextBinding::TypedCovar { ty, .. } => check_type(ty, symbol_table)?,
         }
     }
-    Ok(())
-}
-
-// Checking toplevel declarations
-//
-//
-
-fn check_declaration(decl: &Declaration, symbol_table: &SymbolTable) -> Result<(), Error> {
-    match decl {
-        Declaration::Definition(definition) => check_definition(definition, symbol_table),
-        Declaration::DataDeclaration(data_declaration) => {
-            check_data_declaration(data_declaration, symbol_table)
-        }
-        Declaration::CodataDeclaration(codata_declaration) => {
-            check_codata_declaration(codata_declaration, symbol_table)
-        }
-    }
-}
-
-fn check_definition(def: &Definition, symbol_table: &SymbolTable) -> Result<(), Error> {
-    check_typing_context(&def.context, symbol_table)?;
-    check_type(&def.ret_ty, symbol_table)?;
-    def.body.check(symbol_table, &def.context, &def.ret_ty)
-}
-
-fn check_data_declaration(decl: &DataDeclaration, symbol_table: &SymbolTable) -> Result<(), Error> {
-    for ctor in decl.ctors.iter() {
-        check_ctor_sig(ctor, symbol_table)?;
-    }
-    Ok(())
-}
-
-fn check_ctor_sig(ctor: &CtorSig, symbol_table: &SymbolTable) -> Result<(), Error> {
-    check_typing_context(&ctor.args, symbol_table)?;
-    Ok(())
-}
-
-fn check_codata_declaration(
-    decl: &CodataDeclaration,
-    symbol_table: &SymbolTable,
-) -> Result<(), Error> {
-    for dtor in decl.dtors.iter() {
-        check_dtor_sig(dtor, symbol_table)?;
-    }
-    Ok(())
-}
-
-fn check_dtor_sig(dtor: &DtorSig, symbol_table: &SymbolTable) -> Result<(), Error> {
-    check_typing_context(&dtor.args, symbol_table)?;
-    check_type(&dtor.cont_ty, symbol_table)?;
     Ok(())
 }
 
