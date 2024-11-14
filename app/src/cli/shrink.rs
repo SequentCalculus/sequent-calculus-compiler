d74c1613cf66aef82dc4eef4eff1e91fc7b328a3
mm0821 <mari.mueller@uni-tuebingen.de>
2024-11-15 00:22:26 +0100

    Add CLI options for shrink, linearize and codegen

---
+++ app/src/cli/shrink.rs
@@ -0,0 +1,23 @@
+use core2axcut::program::translate_prog;
+use std::path::PathBuf;
+
+use super::parse_and_check_from_file;
+
+use core::syntax::program::transform_prog;
+use fun2core::program::compile_prog;
+use printer::{ColorChoice, Print, StandardStream};
+
+#[derive(clap::Args)]
+pub struct Args {
+    filepath: PathBuf,
+}
+
+pub fn exec(cmd: Args) -> miette::Result<()> {
+    let parsed = parse_and_check_from_file(cmd.filepath)?;
+    let compiled = compile_prog(parsed);
+    let focused = transform_prog(compiled);
+    let shrunk = translate_prog(focused);
+    let mut stream = Box::new(StandardStream::stdout(ColorChoice::Auto));
+    let _ = shrunk.print_colored(&Default::default(), &mut stream);
+    Ok(())
+}
