6fccd5fb4f9251c40b49740469b0ca8acc05b9d0
David Binder <david.binder@uni-tuebingen.de>
2024-12-03 16:29:28 +0100

    Reenable codespans in miette reports (#102)

--- lang/driver/src/result.rs
+++ lang/driver/src/result.rs
@@ -1,20 +1,13 @@
-use std::fmt::Display;
-
 use thiserror::Error;
 
 use miette::Diagnostic;
 
 #[derive(Error, Diagnostic, Debug, Clone)]
 pub enum DriverError {
-    ParseError(fun::parser::result::ParseError),
-    TypeError(fun::typing::errors::Error),
-}
-
-impl Display for DriverError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            DriverError::ParseError(parse_error) => write!(f, "{parse_error}"),
-            DriverError::TypeError(error) => write!(f, "{error}"),
-        }
-    }
+    #[error(transparent)]
+    #[diagnostic(transparent)]
+    ParseError(#[from] fun::parser::result::ParseError),
+    #[error(transparent)]
+    #[diagnostic(transparent)]
+    TypeError(#[from] fun::typing::errors::Error),
 }
