e35f6b9f0f288072301aeaafc8e99a811484a7c1
David Binder <david.binder@uni-tuebingen.de>
2024-12-05 15:38:30 +0100

    Factor out path logic

--- bench/src/cli/run.rs
+++ bench/src/cli/run.rs
@@ -1,12 +1,15 @@
-use driver::Driver;
+use driver::paths::Paths;
+use driver::{paths, Driver};
 use std::path::PathBuf;
 use std::process::Command;
 
 #[derive(clap::Args)]
 pub struct Args {}
 
+const EXAMPLE: &str = "FibonacciRecursive.sc";
+
 pub fn exec(_cmd: Args) -> miette::Result<()> {
-    let example: PathBuf = "examples/FibonacciRecursive.sc".into();
+    let example: PathBuf = PathBuf::from(paths::EXAMPLES_PATH).join(EXAMPLE);
 
     #[cfg(target_arch = "x86_64")]
     bench_x86_64(&example);
@@ -22,8 +25,12 @@ fn bench_x86_64(example: &PathBuf) {
     let mut drv = Driver::new();
     let _ = drv.compile_x86_64(example, false);
 
+    let mut bin_name = PathBuf::from(EXAMPLE);
+    bin_name.set_extension("");
+    let bin_path = Paths::x86_64_binary_dir().join(bin_name);
+
     Command::new("hyperfine")
-        .arg("target_grk/bin/x86_64/FibonacciRecursive 40")
+        .arg(format!("{} 40", bin_path.to_str().unwrap()))
         .status()
         .expect("Failed to execute hyperfine");
 }
@@ -33,8 +40,12 @@ fn bench_aarch64(example: &PathBuf) {
     let mut drv = Driver::new();
     let _ = drv.compile_aarch64(example, false);
 
+    let mut bin_name = PathBuf::from(EXAMPLE);
+    bin_name.set_extension("");
+    let bin_path = Paths::aarch64_binary_dir().join(bin_name);
+
     Command::new("hyperfine")
-        .arg("target_grk/bin/aarch64/FibonacciRecursive 40")
+        .arg(format!("{} 40", bin_path.to_str().unwrap()))
         .status()
         .expect("Failed to execute hyperfine");
 }
