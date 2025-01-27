1c3953789a0bf2c890bbf0d87622be4658b6add3
MarcoTz <marco.tzschentke@gmail.com>
2025-01-27 19:27:54 +0100

    renamed example and exampleconfig

--- bench/src/cli/config.rs
+++ bench/src/cli/config.rs
@@ -1,22 +1,22 @@
 use std::path::PathBuf;
 
 #[derive(serde::Deserialize)]
-pub struct ExampleConfig {
+pub struct Config {
     pub args: Vec<String>,
     pub runs: u32,
 }
 
-impl Default for ExampleConfig {
-    fn default() -> ExampleConfig {
-        ExampleConfig {
+impl Default for Config {
+    fn default() -> Config {
+        Config {
             args: vec![],
             runs: 10,
         }
     }
 }
 
-impl ExampleConfig {
-    pub fn from_file(path: PathBuf) -> ExampleConfig {
+impl Config {
+    pub fn from_file(path: PathBuf) -> Config {
         let contents = std::fs::read_to_string(path).unwrap();
         basic_toml::from_str(&contents).unwrap_or_default()
     }
