055b9ec4020caaa4d429ff57d89dbb6b76a303f4
mm0821 <mari.mueller@uni-tuebingen.de>
2025-01-27 23:23:58 +0100

    Apply some lints

--- benchmarks/src/cli/run.rs
+++ benchmarks/src/cli/run.rs
@@ -13,17 +13,10 @@ pub fn exec(cmd: Args) -> miette::Result<()> {
 
     for benchmark in benchmarks {
         #[cfg(target_arch = "x86_64")]
-<<<<<<<< HEAD:benchmarks/suite/src/cli/run.rs
         let _ = driver.compile_x86_64(&benchmark.path);
 
         #[cfg(target_arch = "aarch64")]
         let _ = driver.compile_aarch64(&benchmark.path);
-========
-        let _ = driver.compile_x86_64(&benchmark.benchmark_path, false);
-
-        #[cfg(target_arch = "aarch64")]
-        let _ = driver.compile_aarch64(&benchmark.benchmark_path, false);
->>>>>>>> c04bb23 (Change naming a bit):benchmarks/src/cli/run.rs
 
         benchmark.run_hyperfine();
     }
