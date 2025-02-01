8c8a2548b6c2d62f2204aedb9688119993ed3338
mm0821 <mari.mueller@uni-tuebingen.de>
2025-02-01 13:19:03 +0100

    Stop compilers from messing with my names (hopefully)

--- infrastructure/aarch_64/driver.c
+++ infrastructure/aarch_64/driver.c
@@ -4,6 +4,8 @@
 
 #define MAX_DIGITS_INT 20
 
+void println_i64(int64_t value) asm("println_i64");
+
 void println_i64(int64_t value) {
   char buf[MAX_DIGITS_INT + 1];
   char *start = &buf[MAX_DIGITS_INT];
