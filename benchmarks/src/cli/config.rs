9908b3f7eeca567ff3fc8a57b3996f5932348b99
MarcoTz <marco.tzschentke@gmail.com>
2025-04-17 09:49:08 +0200

    moved src dir

--- benchmarks/src/cli/config.rs
+++ benchmarks/src/cli/config.rs
@@ -4,13 +4,18 @@ use std::path::PathBuf;
 pub struct Config {
     pub args: Vec<String>,
     pub runs: u32,
+    pub heap_size: Option<usize>,
+    pub suite: String,
 }
 
 impl Default for Config {
     fn default() -> Config {
+        println!("getting default");
         Config {
             args: vec![],
             runs: 10,
+            heap_size: None,
+            suite: "custom".to_owned(),
         }
     }
 }
@@ -18,6 +23,11 @@ impl Default for Config {
 impl Config {
     pub fn from_file(path: PathBuf) -> Config {
         let contents = std::fs::read_to_string(path).unwrap();
-        basic_toml::from_str(&contents).unwrap_or_default()
+        basic_toml::from_str(&contents)
+            .map_err(|err| {
+                println!("{err}");
+                err
+            })
+            .unwrap_or_default()
     }
 }
