793ed548c407a97814d1336a217791d2e7d53e6e
MarcoTz <marco.tzschentke@gmail.com>
2025-09-04 12:45:45 +0200

    updated flake

--- flake.nix
+++ flake.nix
@@ -1,38 +1,53 @@
 {
   description = "Sequent-Calculus-Compiler";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+  inputs = { 
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; 
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      rustPlatform = pkgs.rustPlatform;
-    in {
-      devShells.${system}.default =
-        pkgs.mkShell { buildInputs = [ self.packages.${system}.default ]; };
-
-      packages.${system}.default = let deps = [ pkgs.yasm pkgs.gcc ];
-      in rustPlatform.buildRustPackage {
-        pname = "Sequent-Calculus-Compiler";
-        nativeBuildInputs = [ pkgs.makeWrapper ];
-        BuildInputs = deps;
-        version = "0.1.0";
-        src = ./.;
-        cargoDeps = rustPlatform.importCargoLock {
-          lockFile = ./Cargo.lock;
-          outputHashes = {
-            "codespan-0.11.1" =
-              "sha256-Wq99v77bqSGIOK/iyv+x/EG1563XSeaTDW5K2X3kSXU=";
-          };
+  outputs = { self, flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        rustPlatform = pkgs.rustPlatform;
+      in {
+        devShells = {
+          sccShell = pkgs.mkShell { buildInputs = [ self.packages.${system}.default ]; };
+          default = self.devShells.${system}.sccShell;
         };
-        postFixup = ''
-          mv $out/bin/scc $out/bin/scc-unwrapped
-          makeWrapper $out/bin/scc-unwrapped $out/bin/scc \
-          --set PATH "${pkgs.lib.makeBinPath deps}";
-        '';
 
-      };
+        packages = {
+            sccPack = 
+              let deps = [ pkgs.yasm pkgs.gcc ];
+              in rustPlatform.buildRustPackage {
+                pname = "Sequent-Calculus-Compiler";
+                nativeBuildInputs = [ pkgs.makeWrapper ];
+                BuildInputs = deps;
+                version = "0.1.0";
+                src = ./.;
+                cargoDeps = rustPlatform.importCargoLock {
+                  lockFile = ./Cargo.lock;
+                  outputHashes = {
+                    "codespan-0.11.1" =
+                      "sha256-Wq99v77bqSGIOK/iyv+x/EG1563XSeaTDW5K2X3kSXU=";
+                  };
+                };
+                postFixup = ''
+                  mv $out/bin/scc $out/bin/scc-unwrapped
+                  makeWrapper $out/bin/scc-unwrapped $out/bin/scc \
+                  --set PATH "${pkgs.lib.makeBinPath deps}";
+                '';
+              };
+          default = self.packages.${system}.sccPack;
+        };
 
-    };
+        apps = {
+          sccApp = {
+            type = "app";
+            program = "${self.packages.${system}.default}/bin/scc";
+          };
+          default = self.apps.${system}.sccApp;
+        };
+      });
 }
