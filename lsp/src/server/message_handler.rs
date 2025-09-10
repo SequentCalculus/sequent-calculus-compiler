669d7c50a25cc1b8f837eab00a00538d2ee8e130
Jason <jmaier2001@gmail.com>
2025-09-10 17:15:18 +0200

    erste Version mit Funktionen für das Lsp ohne den tester

--- lsp/src/server/message_handler.rs
+++ lsp/src/server/message_handler.rs
@@ -2,9 +2,11 @@ use super::{document::Document, method::Method};
 use crate::errors::Error;
 use log::info;
 use lsp_server::{Message, Notification, Request, RequestId, Response};
+//use lsp_types::request::GotoImplementationResponse;
+use lsp_types::request::{/*GotoImplementation,*/ Formatting, GotoImplementationParams, GotoImplementationResponse};
 use lsp_types::{
     DidChangeTextDocumentParams, DidOpenTextDocumentParams, GotoDefinitionParams,
-    GotoDefinitionResponse, PublishDiagnosticsParams, Uri,
+    GotoDefinitionResponse,PublishDiagnosticsParams, Uri, 
 };
 
 pub struct MessageHandler {
@@ -58,9 +60,30 @@ impl MessageHandler {
                 };
                 self.goto_definition(id, params)
             }
+            Method::GotoImplementation => {
+                let (id, params) = match req.extract::<GotoImplementationParams>(&Method::GotoImplementation.to_string())
+                {
+                    Ok(res) => res,
+                    Err(err) => return Error::ExtractReq(err).to_response(req_id),
+                };
+                self.goto_implementation(id, params)
+            }
+
+            
+            //TODO Method::GotoDeclaration
+
+            Method::Formatter => {
+                let (id , params ) = match req.extract::<lsp_types::DocumentFormattingParams>(&"textDocument/formatting".to_string()){
+                    Ok(res) => res,
+                    Err(err) => return Error::ExtractReq(err).to_response(req_id),
+                };
+                self.formatting(id, params)
+            }
+
             _ => {
                 Error::BadRequest(method, "method cannot be request".to_owned()).to_response(req_id)
             }
+
         }
     }
 
@@ -112,6 +135,99 @@ impl MessageHandler {
             error: None,
         }
     }
+
+//eigene Implementation
+    fn goto_implementation(&mut self, id: RequestId, params: GotoImplementationParams) -> Response { 
+        let uri = params.text_document_position_params.text_document.uri;
+        let pos = params.text_document_position_params.position;
+
+        let ident = match self.doc.get_ident(pos) {
+            Ok(id) => id,
+            Err(err) => return err.to_response(id),
+        };
+
+        let loc = match self.doc.find_implementation(ident, uri) { 
+            Ok(loc) => loc,
+            Err(err) => return err.to_response(id),
+        };
+
+        let result = Some(GotoImplementationResponse::Scalar(loc)); 
+        //let result: GotoImplementation::Result = Some(lsp_types::Location::Scalar(loc)); 
+        let result = serde_json::to_value(&result).unwrap();
+        Response {
+            id,
+            result: Some(result),
+            error: None,
+        }
+    }
+/*
+    //eigene Implementation 
+    fn goto_declaration(&mut self, id: RequestId, params: GotoImplementationParams) -> Response {
+
+        let uri = params.text_document_position_params.text_document.uri;
+        let pos = params.text_document_position_params.position;
+
+        let ident = match self.doc.get_ident(pos) {
+            Ok(id) => id,
+            Err(err) => return err.to_response(id),
+        };
+
+        let loc = match self.doc.find_declaration(ident, uri) { 
+            Ok(loc) => loc,
+            Err(err) => return err.to_response(id),
+        };
+
+        let result = Some(GotoDeclarationResponse::Scalar(loc));
+        let result = serde_json::to_value(&result).unwrap();
+        Response {
+            id,
+            result: Some(result),
+            error: None,
+        }
+    }
+
+*/
+
+    fn formatting(&mut self, id: RequestId, params: lsp_types::DocumentFormattingParams,) -> Response {
+        let opts = params.options;
+        
+        let mut formatted = self.doc.get_text().to_string();
+
+        if opts.trim_trailing_whitespace.unwrap_or(false){
+            formatted = formatted.lines().map(|line| line.trim_end()).collect::<Vec<_>>().join("\n");
+        }
+
+        if opts.trim_final_newlines.unwrap_or(false){
+            while formatted.ends_with("\n\n"){ 
+                formatted.pop();
+            }
+        }
+
+        if opts.insert_final_newline.unwrap_or(false) && !formatted.ends_with('\n') {
+            formatted.push('\n');
+        }
+
+        let edit = lsp_types::TextEdit {
+            range: lsp_types::Range { 
+                start: lsp_types::Position { line: 0, character: 0 },
+                end: lsp_types::Position { line: u32::MAX, character: u32::MAX }
+            },
+            new_text: formatted,
+        };
+
+
+        let text_edit = vec![edit];
+        let result = serde_json::to_value(text_edit).unwrap();
+
+        Response {
+            id,
+            result: Some(result),
+            error: None,
+        }
+    }
+
+
+    
 
     fn did_open(&mut self, params: DidOpenTextDocumentParams) -> Notification {
         match Document::from_text(params.text_document.text) {
