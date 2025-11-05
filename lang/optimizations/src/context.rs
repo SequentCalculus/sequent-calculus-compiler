8f9121fa0590e7e6235b62765bf63460ac2365c6
MarcoTz <marco.tzschentke@gmail.com>
2025-11-05 19:35:49 +0100

    implemented first inlining

--- lang/optimizations/src/context.rs
+++ lang/optimizations/src/context.rs
@@ -1,26 +1,26 @@
-use super::{Inline, InlineContext};
+use super::{Error, Inline, InlineContext};
 use axcut::syntax::{ContextBinding, TypingContext};
 
 impl Inline for TypingContext {
     type Target = TypingContext;
-    fn inline(self, ctx: &mut InlineContext) -> Self::Target {
-        TypingContext {
+    fn inline(self, ctx: &mut InlineContext) -> Result<Self::Target, Error> {
+        Ok(TypingContext {
             bindings: self
                 .bindings
                 .into_iter()
                 .map(|bind| bind.inline(ctx))
-                .collect(),
-        }
+                .collect::<Result<Vec<_>, Error>>()?,
+        })
     }
 }
 
 impl Inline for ContextBinding {
     type Target = ContextBinding;
-    fn inline(self, ctx: &mut InlineContext) -> Self::Target {
-        ContextBinding {
+    fn inline(self, ctx: &mut InlineContext) -> Result<Self::Target, Error> {
+        Ok(ContextBinding {
             var: self.var,
             chi: self.chi,
             ty: self.ty,
-        }
+        })
     }
 }
