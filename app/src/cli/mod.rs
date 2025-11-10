dd0c019a603f24988c67427e53ec8013e02566dd
MarcoTz <marco.tzschentke@gmail.com>
2025-11-10 15:17:47 +0100

    added no-color option

--- app/src/cli/mod.rs
+++ app/src/cli/mod.rs
@@ -1,6 +1,7 @@
 //! This module defines the command-line interface of the `scc` compiler.
 
 use clap::{Parser, Subcommand};
+use printer::{ColorChoice, Print, PrintCfg, StandardStream};
 
 mod check;
 mod clean;
@@ -14,6 +15,18 @@ mod linearize;
 mod shrink;
 mod texify;
 
+pub fn print_res<T>(t: &T, color: bool)
+where
+    T: Print,
+{
+    let mut stream = Box::new(StandardStream::stdout(ColorChoice::Auto));
+    if color {
+        let _ = t.print_colored(&PrintCfg::default(), &mut stream);
+    } else {
+        println!("{}", t.print_to_string(Some(&PrintCfg::default())));
+    }
+}
+
 /// This functions executes the compiler. It parsed the command to perform and then performs it.
 pub fn exec() -> miette::Result<()> {
     use Command::*;
@@ -22,14 +35,14 @@ pub fn exec() -> miette::Result<()> {
         Check(args) => check::exec(args),
         Clean(args) => clean::exec(args),
         Codegen(args) => codegen::exec(args),
-        Compile(args) => compile::exec(args),
-        Focus(args) => focus::exec(args),
+        Compile(args) => compile::exec(args, !cli.no_color),
+        Focus(args) => focus::exec(args, !cli.no_color),
         Fmt(args) => fmt::exec(args),
-        Linearize(args) => linearize::exec(args),
-        Shrink(args) => shrink::exec(args),
+        Linearize(args) => linearize::exec(args, !cli.no_color),
+        Shrink(args) => shrink::exec(args, !cli.no_color),
         Texify(args) => texify::exec(args),
         GenerateCompletion(args) => gen_completions::exec(args),
-        Inline(args) => inline::exec(args),
+        Inline(args) => inline::exec(args, !cli.no_color),
     }
 }
 
@@ -39,6 +52,8 @@ pub fn exec() -> miette::Result<()> {
 struct Cli {
     #[clap(subcommand)]
     command: Command,
+    #[clap(short, long)]
+    no_color: bool,
 }
 
 /// This enum encodes the commands the compiler can execute.
