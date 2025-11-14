6985a4fd71bc6dd9a2991eeccee1c703b2a73918
MarcoTz <marco.tzschentke@gmail.com>
2025-11-14 10:51:57 +0100

    fixed rewriting recursively and added example to test

--- lang/optimizations/src/statement/let.rs
+++ lang/optimizations/src/statement/let.rs
@@ -5,6 +5,13 @@ impl Rewrite for Let {
     type Target = Self;
     fn rewrite(self, ctx: &mut RewriteContext) -> Result<Self::Target, Error> {
         ctx.add_binding(&self);
-        Ok(self)
+        Ok(Let {
+            var: self.var,
+            ty: self.ty,
+            tag: self.tag,
+            args: self.args,
+            next: self.next.rewrite(ctx)?,
+            free_vars_next: self.free_vars_next,
+        })
     }
 }
