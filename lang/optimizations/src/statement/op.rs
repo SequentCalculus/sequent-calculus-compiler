6279ab6791bcea722a5d98bc858d88bf950f4200
MarcoTz <marco.tzschentke@gmail.com>
2025-11-14 10:51:57 +0100

    fixed rewriting recursively and added example to test

--- lang/optimizations/src/statement/op.rs
+++ lang/optimizations/src/statement/op.rs
@@ -3,7 +3,14 @@ use axcut::syntax::statements::Op;
 
 impl Rewrite for Op {
     type Target = Self;
-    fn rewrite(self, _: &mut RewriteContext) -> Result<Self::Target, Error> {
-        Ok(self)
+    fn rewrite(self, ctx: &mut RewriteContext) -> Result<Self::Target, Error> {
+        Ok(Op {
+            fst: self.fst,
+            op: self.op,
+            snd: self.snd,
+            var: self.var,
+            next: self.next.rewrite(ctx)?,
+            free_vars_next: self.free_vars_next,
+        })
     }
 }
--- lang/optimizations/src/statement/print.rs
+++ lang/optimizations/src/statement/print.rs
@@ -3,7 +3,12 @@ use axcut::syntax::statements::PrintI64;
 
 impl Rewrite for PrintI64 {
     type Target = Self;
-    fn rewrite(self, _: &mut RewriteContext) -> Result<Self::Target, Error> {
-        Ok(self)
+    fn rewrite(self, ctx: &mut RewriteContext) -> Result<Self::Target, Error> {
+        Ok(PrintI64 {
+            newline: self.newline,
+            var: self.var,
+            next: self.next.rewrite(ctx)?,
+            free_vars_next: self.free_vars_next,
+        })
     }
 }
