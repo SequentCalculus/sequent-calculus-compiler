c09707a8e3e7fc99b3f97488abd52d34ab14118d
MarcoTz <marco.tzschentke@gmail.com>
2025-11-14 11:41:58 +0100

    added max_runs and new changes

--- lang/optimizations/src/context.rs
+++ lang/optimizations/src/context.rs
@@ -3,12 +3,14 @@ use std::collections::HashMap;
 
 pub struct RewriteContext {
     let_bindings: HashMap<Var, Let>,
+    pub new_changes: bool,
 }
 
 impl RewriteContext {
     pub fn new() -> Self {
         Self {
             let_bindings: HashMap::new(),
+            new_changes: false,
         }
     }
 
@@ -18,11 +20,5 @@ impl RewriteContext {
 
     pub fn get_binding(&self, var: &Var) -> Option<Let> {
         self.let_bindings.get(var).cloned()
-    }
-}
-
-impl Default for RewriteContext {
-    fn default() -> RewriteContext {
-        RewriteContext::new()
     }
 }
