57c8f2c5e9ec8332aae054c195f3ae5b56a317fc
MarcoTz <marco.tzschentke@gmail.com>
2025-11-14 10:13:14 +0100

    added rewriting let-switch pairs

--- lang/optimizations/src/errors.rs
+++ lang/optimizations/src/errors.rs
@@ -1,15 +1,31 @@
+use axcut::syntax::{Name, statements::Switch};
 use miette::Diagnostic;
 use std::fmt;
 
 #[derive(Debug, Clone, Diagnostic)]
 pub enum Error {
-    UnknownDeclaration { name: String },
+    NoMatchingClause {
+        switch: String,
+        xtor: Name,
+    },
+    ClauseArity {
+        clause_args: usize,
+        xtor_args: usize,
+    },
 }
 
 impl Error {
-    pub fn unknown(name: &str) -> Error {
-        Error::UnknownDeclaration {
-            name: name.to_owned(),
+    pub fn clause(switch: &Switch, xtor: &str) -> Error {
+        Error::NoMatchingClause {
+            switch: format!("{switch:#?}"),
+            xtor: xtor.to_owned(),
+        }
+    }
+
+    pub fn arity(clause: usize, xtor: usize) -> Error {
+        Error::ClauseArity {
+            clause_args: clause,
+            xtor_args: xtor,
         }
     }
 }
@@ -17,9 +33,16 @@ impl Error {
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Error::UnknownDeclaration { name } => {
-                write!(f, "Could not find a type definition for {name}")
+            Error::NoMatchingClause { switch, xtor } => {
+                write!(f, "Could not find clause for {xtor} in {switch}")
             }
+            Error::ClauseArity {
+                clause_args,
+                xtor_args,
+            } => write!(
+                f,
+                "Arity Mismatch: clause has {clause_args} bindings, xtor has {xtor_args}"
+            ),
         }
     }
 }
