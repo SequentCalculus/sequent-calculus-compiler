d0573a2c0c15b69b8000b11c682d62f786df661c
MarcoTz <marco.tzschentke@gmail.com>
2025-11-14 11:25:46 +0100

    added calculating free vars and removing unused lets

--- lang/optimizations/src/statement/let.rs
+++ lang/optimizations/src/statement/let.rs
@@ -1,17 +1,28 @@
 use crate::{Error, Rewrite, RewriteContext};
-use axcut::syntax::statements::Let;
+use axcut::{
+    syntax::statements::{Let, Statement},
+    traits::free_vars::FreeVars,
+};
+use std::{collections::HashSet, rc::Rc};
 
 impl Rewrite for Let {
-    type Target = Self;
+    type Target = Statement;
     fn rewrite(self, ctx: &mut RewriteContext) -> Result<Self::Target, Error> {
         ctx.add_binding(&self);
-        Ok(Let {
-            var: self.var,
-            ty: self.ty,
-            tag: self.tag,
-            args: self.args,
-            next: self.next.rewrite(ctx)?,
-            free_vars_next: self.free_vars_next,
-        })
+        let mut free_vars_next = HashSet::new();
+        let new_next = self.next.rewrite(ctx)?.free_vars(&mut free_vars_next);
+        if !free_vars_next.contains(&self.var) {
+            Ok(Rc::unwrap_or_clone(new_next))
+        } else {
+            Ok(Let {
+                var: self.var,
+                ty: self.ty,
+                tag: self.tag,
+                args: self.args,
+                next: new_next,
+                free_vars_next: Some(free_vars_next),
+            }
+            .into())
+        }
     }
 }
--- lang/optimizations/src/statement/op.rs
+++ lang/optimizations/src/statement/op.rs
@@ -1,16 +1,19 @@
 use crate::{Error, Rewrite, RewriteContext};
-use axcut::syntax::statements::Op;
+use axcut::{syntax::statements::Op, traits::free_vars::FreeVars};
+use std::collections::HashSet;
 
 impl Rewrite for Op {
     type Target = Self;
     fn rewrite(self, ctx: &mut RewriteContext) -> Result<Self::Target, Error> {
+        let mut free_next = HashSet::new();
+        let new_next = self.next.rewrite(ctx)?.free_vars(&mut free_next);
         Ok(Op {
             fst: self.fst,
             op: self.op,
             snd: self.snd,
             var: self.var,
-            next: self.next.rewrite(ctx)?,
-            free_vars_next: self.free_vars_next,
+            next: new_next,
+            free_vars_next: Some(free_next),
         })
     }
 }
