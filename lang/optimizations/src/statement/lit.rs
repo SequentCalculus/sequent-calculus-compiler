405e633fdee04c6ae484ca3309875dd231faaef8
MarcoTz <marco.tzschentke@gmail.com>
2025-11-14 11:25:46 +0100

    added calculating free vars and removing unused lets

--- lang/optimizations/src/statement/lit.rs
+++ lang/optimizations/src/statement/lit.rs
@@ -1,14 +1,17 @@
 use crate::{Error, Rewrite, RewriteContext};
-use axcut::syntax::statements::Literal;
+use axcut::{syntax::statements::Literal, traits::free_vars::FreeVars};
+use std::collections::HashSet;
 
 impl Rewrite for Literal {
     type Target = Self;
     fn rewrite(self, ctx: &mut RewriteContext) -> Result<Self::Target, Error> {
+        let mut free_next = HashSet::new();
+        let new_next = self.next.rewrite(ctx)?.free_vars(&mut free_next);
         Ok(Literal {
             lit: self.lit,
             var: self.var,
-            next: self.next.rewrite(ctx)?,
-            free_vars_next: self.free_vars_next,
+            next: new_next,
+            free_vars_next: Some(free_next),
         })
     }
 }
