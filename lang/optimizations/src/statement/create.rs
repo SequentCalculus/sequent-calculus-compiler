a1037af4068b5033340825fa277411a54d29f20e
MarcoTz <marco.tzschentke@gmail.com>
2025-11-21 11:48:21 +0100

    added replacing create clauses with lifted calls

--- lang/optimizations/src/statement/create.rs
+++ lang/optimizations/src/statement/create.rs
@@ -1,5 +1,5 @@
 use crate::{Error, Rewrite, RewriteContext, free_bindings::FreeBindings};
-use axcut::syntax::statements::{Create, Statement};
+use axcut::syntax::statements::{Call, Clause, Create, Statement};
 use std::rc::Rc;
 
 impl Rewrite for Create {
@@ -8,7 +8,38 @@ impl Rewrite for Create {
         ctx.add_create(&self);
         let mut new_clauses = vec![];
         for clause in self.clauses {
-            let new_clause = clause.rewrite(ctx)?;
+            let lifted_name = ctx.lifted_name(&clause.xtor, &self.var);
+            let new_clause;
+            if ctx.already_lifted(&lifted_name) {
+                let new_def = ctx
+                    .rewritten_defs
+                    .iter()
+                    .find(|def| def.name == lifted_name)
+                    .ok_or(Error::DefinitionNotFound {
+                        name: lifted_name.clone(),
+                    })?;
+                let mut new_args = clause.context.clone();
+                let remaining_args = new_def
+                    .context
+                    .bindings
+                    .clone()
+                    .split_off(new_args.bindings.len());
+                new_args.bindings.extend(remaining_args);
+                new_clause = Clause {
+                    xtor: clause.xtor,
+                    context: clause.context,
+                    body: Rc::new(
+                        Call {
+                            label: lifted_name,
+                            args: new_args,
+                        }
+                        .into(),
+                    ),
+                };
+            } else {
+                new_clause = clause.rewrite(ctx)?;
+            }
+
             new_clauses.push(new_clause);
         }
         let new_next = self.next.rewrite(ctx)?;
