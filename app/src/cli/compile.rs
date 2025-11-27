bd73af6a3792612d491a27b4cd2d7346e3d4a640
mm0821 <69968840+mm0821@users.noreply.github.com>
2025-11-27 12:50:06 +0100

    Add option for printing to stdout without colors (#211)

--- app/src/cli/compile.rs
+++ app/src/cli/compile.rs
@@ -1,16 +1,16 @@
 //! This module contains the command for compiling a file to Core.
 
+use super::print_stdout;
 use std::path::PathBuf;
 
 use driver::{Driver, PrintMode};
-use printer::{ColorChoice, Print, PrintCfg, StandardStream};
 
 #[derive(clap::Args)]
 pub struct Args {
     filepath: PathBuf,
 }
 
-pub fn exec(cmd: Args) -> miette::Result<()> {
+pub fn exec(cmd: Args, colored: bool) -> miette::Result<()> {
     let mut drv = Driver::new();
     let compiled = drv.compiled(&cmd.filepath);
     let compiled = match compiled {
@@ -18,8 +18,6 @@ pub fn exec(cmd: Args) -> miette::Result<()> {
         Err(err) => return Err(drv.error_to_report(err, &cmd.filepath)),
     };
     drv.print_compiled(&cmd.filepath, PrintMode::Textual)?;
-
-    let mut stream = Box::new(StandardStream::stdout(ColorChoice::Auto));
-    let _ = compiled.print_colored(&PrintCfg::default(), &mut stream);
+    print_stdout(&compiled, colored);
     Ok(())
 }
--- app/src/cli/focus.rs
+++ app/src/cli/focus.rs
@@ -2,15 +2,15 @@
 
 use std::path::PathBuf;
 
+use super::print_stdout;
 use driver::{Driver, PrintMode};
-use printer::{ColorChoice, Print, PrintCfg, StandardStream};
 
 #[derive(clap::Args)]
 pub struct Args {
     filepath: PathBuf,
 }
 
-pub fn exec(cmd: Args) -> miette::Result<()> {
+pub fn exec(cmd: Args, colored: bool) -> miette::Result<()> {
     let mut drv = Driver::new();
     let focused = drv.focused(&cmd.filepath);
     let focused = match focused {
@@ -18,8 +18,6 @@ pub fn exec(cmd: Args) -> miette::Result<()> {
         Err(err) => return Err(drv.error_to_report(err, &cmd.filepath)),
     };
     drv.print_focused(&cmd.filepath, PrintMode::Textual)?;
-
-    let mut stream = Box::new(StandardStream::stdout(ColorChoice::Auto));
-    let _ = focused.print_colored(&PrintCfg::default(), &mut stream);
+    print_stdout(&focused, colored);
     Ok(())
 }
