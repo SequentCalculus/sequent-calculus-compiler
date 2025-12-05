722e989d217ae73024fb045cce402ec276a0b612
MarcoTz <marco.tzschentke@gmail.com>
2025-12-05 13:43:49 +0100

    added inlining leaf statements

--- lang/optimizations/src/program.rs
+++ lang/optimizations/src/program.rs
@@ -1,24 +1,38 @@
-use crate::def::rewrite_def;
+use crate::{def::rewrite_def, rewrite::RewriteState};
 use axcut::syntax::Prog;
+use std::{
+    collections::{HashMap, HashSet},
+    mem::take,
+};
 
 pub const MAX_RUNS: u64 = 1;
 
 pub fn rewrite_prog(mut program: Prog) -> Prog {
     // we thread the set of labels of top-level functions through the translation, because we need
     // to generate fresh labels when we lift statements
-    let mut used_labels = program.defs.iter().map(|def| def.name.clone()).collect();
+    let used_labels = program.defs.iter().map(|def| def.name.clone()).collect();
+    let defs = take(&mut program.defs);
+    let mut state = RewriteState {
+        used_labels: used_labels,
+        lifted_statements: defs,
+        current_label: "main".to_owned(),
+        current_used_vars: HashSet::new(),
+        let_bindings: HashMap::new(),
+        create_bindings: HashMap::new(),
+        new_changes: true,
+    };
 
     let mut performed_runs = 0;
-    let mut new_changes = true;
-    while new_changes && performed_runs < MAX_RUNS {
-        new_changes = false;
+    while state.new_changes && performed_runs < MAX_RUNS {
+        state.new_changes = false;
         performed_runs += 1;
-        program.defs = program
-            .defs
-            .into_iter()
-            .flat_map(|def| rewrite_def(def, &mut used_labels, &mut new_changes))
-            .collect();
+        let mut current_labels = state.used_labels.iter().cloned().collect::<Vec<_>>();
+        current_labels.sort();
+        for def_name in current_labels.iter() {
+            rewrite_def(def_name, &mut state);
+        }
     }
 
+    program.defs = state.lifted_statements;
     program
 }
