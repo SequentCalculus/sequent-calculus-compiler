ddccadaa5055f97df8e7a63384e6aeae36dc764d
MarcoTz <marco.tzschentke@gmail.com>
2025-12-08 17:12:24 +0100

    added checking out different branches

--- evaluation/src/errors.rs
+++ evaluation/src/errors.rs
@@ -5,8 +5,32 @@ use std::{
 
 #[derive(Debug)]
 pub enum Error {
-    ReadConfig { path: PathBuf, msg: String },
-    Toml { path: PathBuf, msg: String },
+    ReadConfig {
+        path: PathBuf,
+        msg: String,
+    },
+    Toml {
+        path: PathBuf,
+        msg: String,
+    },
+    Command {
+        cmd: String,
+        tried: String,
+        msg: String,
+    },
+    ParseStdOut {
+        cmd: String,
+        msg: String,
+    },
+    CreateDir {
+        path: PathBuf,
+        msg: String,
+    },
+    MoveFile {
+        from: PathBuf,
+        to: PathBuf,
+        msg: String,
+    },
 }
 
 impl Error {
@@ -29,6 +53,48 @@ impl Error {
             msg: err.to_string(),
         }
     }
+
+    pub fn cmd<T>(cmd: &str, tried: &str, err: T) -> Error
+    where
+        T: fmt::Display,
+    {
+        Error::Command {
+            cmd: cmd.to_owned(),
+            tried: tried.to_owned(),
+            msg: err.to_string(),
+        }
+    }
+
+    pub fn parse_out<Err>(cmd: &str, err: Err) -> Error
+    where
+        Err: std::error::Error,
+    {
+        Error::ParseStdOut {
+            cmd: cmd.to_owned(),
+            msg: err.to_string(),
+        }
+    }
+
+    pub fn create_dir<Err>(path: &Path, err: Err) -> Error
+    where
+        Err: std::error::Error,
+    {
+        Error::CreateDir {
+            path: path.to_path_buf(),
+            msg: err.to_string(),
+        }
+    }
+
+    pub fn move_file<Err>(from: &Path, to: &Path, err: Err) -> Error
+    where
+        Err: std::error::Error,
+    {
+        Error::MoveFile {
+            from: from.to_path_buf(),
+            to: to.to_path_buf(),
+            msg: err.to_string(),
+        }
+    }
 }
 
 impl fmt::Display for Error {
@@ -40,6 +106,22 @@ impl fmt::Display for Error {
             Error::Toml { path, msg } => {
                 write!(f, "Could not read toml {}:\n{msg}", path.display())
             }
+            Error::Command { cmd, tried, msg } => write!(
+                f,
+                "Could not {tried}, command {cmd} exited with error {msg}"
+            ),
+            Error::ParseStdOut { cmd, msg } => {
+                write!(f, "Could not read std out from {cmd}: {msg}")
+            }
+            Error::CreateDir { path, msg } => {
+                write!(f, "Could not create directory {}:{msg}", path.display())
+            }
+            Error::MoveFile { from, to, msg } => write!(
+                f,
+                "Could not move {} -> {}: {msg}",
+                from.display(),
+                to.display()
+            ),
         }
     }
 }
--- evaluation/src/main.rs
+++ evaluation/src/main.rs
@@ -1,21 +1,83 @@
-use std::{fs::read_to_string, path::PathBuf};
+use std::{
+    fs::{create_dir_all, read_to_string, rename},
+    path::PathBuf,
+    process::Command,
+};
 
 mod errors;
 use errors::Error;
 
 const EVAL_CONFIG: &str = "evaluation/config.toml";
+const SCC_BIN: &str = "target/release/scc";
+const BIN_OUT: &str = "target_scc/versions";
 
-#[derive(Clone, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Deserialize)]
 struct EvalConfig {
     version_git_hashes: Vec<String>,
 }
 
+impl EvalConfig {
+    pub fn load() -> Result<EvalConfig, Error> {
+        let config_path = PathBuf::from(EVAL_CONFIG);
+        let config_contents =
+            read_to_string(&config_path).map_err(|err| Error::read_conf(&config_path, err))?;
+        basic_toml::from_str::<EvalConfig>(&config_contents)
+            .map_err(|err| Error::toml(&config_path, err))
+    }
+}
+
 fn main() -> Result<(), Error> {
-    println!("{}", std::env::current_dir().unwrap().display());
-    let config_path = PathBuf::from(EVAL_CONFIG);
-    let config_contents =
-        read_to_string(&config_path).map_err(|err| Error::read_conf(&config_path, err))?;
-    let config = basic_toml::from_str::<EvalConfig>(&config_contents)
-        .map_err(|err| Error::toml(&config_path, err))?;
+    let config = EvalConfig::load()?;
+    compile_versions(&config.version_git_hashes)?;
+    Ok(())
+}
+
+fn compile_versions(hashes: &[String]) -> Result<(), Error> {
+    let current_branch = String::from_utf8(
+        Command::new("git")
+            .arg("branch")
+            .arg("--show-current")
+            .output()
+            .map_err(|err| Error::cmd("git", "get current branch", err))?
+            .stdout,
+    )
+    .map_err(|err| Error::parse_out("git branch", err))?;
+
+    let bin_path = PathBuf::from(BIN_OUT);
+    create_dir_all(&bin_path).map_err(|err| Error::create_dir(&bin_path, err))?;
+
+    let compiled_path = PathBuf::from(SCC_BIN);
+
+    for (index, hash) in hashes.iter().enumerate() {
+        let checkout_res = Command::new("git")
+            .arg("checkout")
+            .arg(hash)
+            .status()
+            .map_err(|err| Error::cmd("git", "checkout version branch", err))?;
+        if !checkout_res.success() {
+            return Err(Error::cmd(
+                "git",
+                "checkout version branch",
+                format!("exited with code {checkout_res}"),
+            ));
+        }
+
+        let out_path = bin_path.join(format!("scc_{index}"));
+        rename(&compiled_path, &out_path)
+            .map_err(|err| Error::move_file(&compiled_path, &out_path, err))?;
+    }
+
+    let checkout_res = Command::new("git")
+        .arg("checkout")
+        .arg(current_branch.trim())
+        .status()
+        .map_err(|err| Error::cmd("git", "checkout current branch", err))?;
+    if !checkout_res.success() {
+        return Err(Error::cmd(
+            "git",
+            "checkout current branch",
+            format!("exited with {}", checkout_res),
+        ));
+    }
     Ok(())
 }
