eb615356208b82a28092d3afd466cfa79f2c99c8
MarcoTz <marco.tzschentke@gmail.com>
2025-12-08 22:15:36 +0100

    debug output

--- evaluation/src/examples.rs
+++ evaluation/src/examples.rs
@@ -172,6 +172,7 @@ fn update_results(results: &mut Vec<EvalResult>, stdout: String, example_name: &
     let mut num_create = None;
     let mut num_switch = None;
     for line in stdout.lines() {
+        println!("{line}");
         if line.contains("Number of Passes") {
             let mut line_parts = line.split(":");
             line_parts.next();
