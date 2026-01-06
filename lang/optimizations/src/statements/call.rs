7f955046516d955963ad5e87bfa8122da96ae02c
mm0821 <mari.mueller@uni-tuebingen.de>
2026-01-06 18:14:30 +0100

    Enable rewriting of direct recursive calls
    For top-level definitions whose body is a switch, we lift all the
    clauses not consisting of a leaf statement. This way, direct recursive
    that would trigger a rewrite become calls in lifted definitions. For the
    clauses unnecessarily lifted by this, we rely on a yet to be
    implemented) cleanup inlining pass to undo the lifting.

--- lang/optimizations/src/statements/call.rs
+++ lang/optimizations/src/statements/call.rs
@@ -8,54 +8,48 @@ use std::rc::Rc;
 impl Rewrite for Call {
     type Target = Statement;
     fn rewrite(mut self, state: &mut RewriteState) -> Self::Target {
-        if self.label == state.current_label {
-            // TODO: also rewrite direct recursive calls
-            self.into()
+        // here we temporarily remove the Switch from its definition
+        let Some(mut switch_info) = state.get_switch_info(&self.label, &self.args) else {
+            return self.into();
+        };
+        state.new_changes = true;
+
+        let clause = &switch_info.switch.clauses[switch_info.clause_position];
+        let result = if matches!(
+            &*clause.body,
+            Statement::Call(_) | Statement::Invoke(_) | Statement::Exit(_)
+        ) {
+            // for leaf statements, we do not lift, but just substitute into them
+            let subst = clause
+                .context
+                .iter_vars_cloned()
+                .zip(switch_info.let_args.iter_vars_cloned())
+                .chain(
+                    state.defs[switch_info.called_def_position]
+                        .context
+                        .iter_vars_cloned()
+                        .zip(self.args.iter_vars_cloned()),
+                )
+                .collect::<Vec<_>>();
+
+            Rc::unwrap_or_clone(clause.body.clone().subst_sim(&subst))
         } else {
-            // here we temporarily remove the Switch from its definition
-            let Some(mut switch_info) = state.get_switch_info(&self.label, &self.args) else {
-                return self.into();
-            };
-            state.new_changes = true;
+            let (label, arg_positions) = state.lift_switch_clause(&mut switch_info, &self.label);
+            self.args.bindings.extend(switch_info.let_args.bindings);
+            let mut args = Vec::with_capacity(arg_positions.len());
+            for position in arg_positions {
+                args.push(self.args.bindings[position].clone());
+            }
 
-            let clause = &switch_info.switch.clauses[switch_info.clause_position];
-            let result = if matches!(
-                &*clause.body,
-                Statement::Call(_) | Statement::Invoke(_) | Statement::Exit(_)
-            ) {
-                // for leaf statements, we do not lift, but just substitute into them
-                let subst = clause
-                    .context
-                    .iter_vars_cloned()
-                    .zip(switch_info.let_args.iter_vars_cloned())
-                    .chain(
-                        state.defs[switch_info.called_def_position]
-                            .context
-                            .iter_vars_cloned()
-                            .zip(self.args.iter_vars_cloned()),
-                    )
-                    .collect::<Vec<_>>();
+            Call {
+                label,
+                args: args.into(),
+            }
+            .into()
+        };
 
-                Rc::unwrap_or_clone(clause.body.clone().subst_sim(&subst))
-            } else {
-                let (label, arg_positions) =
-                    state.lift_switch_clause(&mut switch_info, &self.label);
-                self.args.bindings.extend(switch_info.let_args.bindings);
-                let mut args = Vec::with_capacity(arg_positions.len());
-                for position in arg_positions {
-                    args.push(self.args.bindings[position].clone());
-                }
-
-                Call {
-                    label,
-                    args: args.into(),
-                }
-                .into()
-            };
-
-            // now we have to put the (potentially lifted) Switch back into place
-            state.defs[switch_info.called_def_position].body = switch_info.switch.into();
-            result
-        }
+        // now we have to put the (potentially lifted) Switch back into place
+        state.defs[switch_info.called_def_position].body = switch_info.switch.into();
+        result
     }
 }
