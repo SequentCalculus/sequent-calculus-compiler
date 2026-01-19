709c0d315e23a1e4dee17d20c2aece0f701e1df1
MarcoTz <marco.tzschentke@gmail.com>
2026-01-19 18:38:03 +0100

    fixed macro and started changing tests

--- testsuite/src/macros.rs
+++ testsuite/src/macros.rs
@@ -4,11 +4,15 @@ use core_lang::syntax::{
     terms::{xtor::Xtor, xvar::XVar},
     types::Ty,
 };
-use macros::{ctor, cut, dtor};
+use macros::{ctor, cut, dtor, ty};
 
 #[test]
 fn cut_macro() {
-    let cut1 = cut!(XVar::var("x", Ty::I64), XVar::covar("a", Ty::I64), Ty::I64);
+    let cut1 = cut!(
+        XVar::var("x", Ty::I64),
+        XVar::covar("a", Ty::I64),
+        ty!("int")
+    );
     let cut2 = Cut::new(XVar::var("x", Ty::I64), XVar::covar("a", Ty::I64), Ty::I64);
     assert_eq!(cut1, cut2)
 }
@@ -17,12 +21,10 @@ fn cut_macro() {
 fn list_int() {
     let list1 = ctor!(
         "Cons",
-        [
-            XVar::var("x", Ty::I64),
-            ctor!("Nil", [], Ty::Decl("ListInt".to_string())),
-        ],
-        Ty::Decl("ListInt".to_string())
+        [XVar::var("x", ty!("int")), ctor!("Nil", [], ty!("ListInt")),],
+        ty!("ListInt")
     );
+
     let mut arguments = Arguments::default();
     arguments.add_prod(XVar::var("x", Ty::I64));
     arguments.add_prod(Xtor::ctor(
@@ -36,11 +38,7 @@ fn list_int() {
 
 #[test]
 fn fun_int() {
-    let fun1 = dtor!(
-        "apply",
-        [XVar::var("x", Ty::I64)],
-        Ty::Decl("FunI64I64".to_string())
-    );
+    let fun1 = dtor!("apply", [XVar::var("x", ty!("int"))], ty!("FunI64I64"));
     let mut arguments = Arguments::default();
     arguments.add_prod(XVar::var("x", Ty::I64));
     let fun2 = Xtor::dtor("apply", arguments, Ty::Decl("FunI64I64".to_string()));
