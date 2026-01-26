4735e27d70ce86cfe5a64216cbc238dde0977a56
MarcoTz <marco.tzschentke@gmail.com>
2026-01-26 16:02:45 +0100

    updated rv64 tests

--- lang/axcut2rv64/tests/mini.rs
+++ lang/axcut2rv64/tests/mini.rs
@@ -1,80 +1,23 @@
-use axcut::syntax::statements::*;
-use axcut::syntax::*;
 use axcut2backend::coder::compile;
 use axcut2rv64::Backend;
 use axcut2rv64::into_routine::into_rv64_routine;
 use goldenfile::Mint;
-use std::collections::HashSet;
 use std::io::prelude::*;
-use std::rc::Rc;
+
+use axcut_macros::{bind, call, def, exit, lit, prog, sum};
 
 #[test]
 fn test_mini() {
-    let main_body = Statement::Call(Call {
-        label: "l".to_string(),
-        args: vec![].into(),
-    });
-    let main = Def {
-        name: "main".to_string(),
-        context: Vec::new().into(),
-        body: main_body,
-        used_vars: HashSet::new(),
-    };
+    let main_body = call!("l", []);
+    let main = def!("main", [], main_body);
 
-    let l_body = Statement::Literal(Literal {
-        lit: 1,
-        var: "x".to_string(),
-        next: Rc::new(Statement::Literal(Literal {
-            lit: 9,
-            var: "y".to_string(),
-            next: Rc::new(Statement::Call(Call {
-                label: "j".to_string(),
-                args: vec![].into(),
-            })),
-            free_vars_next: None,
-        })),
-        free_vars_next: None,
-    });
-    let l = Def {
-        name: "l".to_string(),
-        context: Vec::new().into(),
-        body: l_body,
-        used_vars: HashSet::new(),
-    };
+    let l_body = lit!(1, "x", lit!(9, "y", call!("j", [])));
+    let l = def!("l", [], l_body);
 
-    let j_body = Statement::Op(Op {
-        fst: "x".to_string(),
-        op: BinOp::Sum,
-        snd: "y".to_string(),
-        var: "z".to_string(),
-        next: Rc::new(Statement::Exit(Exit {
-            var: "z".to_string(),
-        })),
-        free_vars_next: None,
-    });
-    let j = Def {
-        name: "j".to_string(),
-        context: vec![
-            ContextBinding {
-                var: "y".to_string(),
-                chi: Chirality::Ext,
-                ty: Ty::I64,
-            },
-            ContextBinding {
-                var: "x".to_string(),
-                chi: Chirality::Ext,
-                ty: Ty::I64,
-            },
-        ]
-        .into(),
-        body: j_body,
-        used_vars: HashSet::new(),
-    };
+    let j_body = sum!("x", "y", "z", exit!("z"));
+    let j = def!("j", [bind!("y"), bind!("x")], j_body);
 
-    let program = Prog {
-        defs: vec![main, l, j],
-        types: Vec::new(),
-    };
+    let program = prog!([main, l, j], []);
 
     let assembly_prog = compile::<Backend, _, _, _>(program);
     let assembler_code = into_rv64_routine(assembly_prog);
