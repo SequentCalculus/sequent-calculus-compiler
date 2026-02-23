b2f566c17430ec22928a3bc4fd9adcef8324fe95
MarcoTz <marco.tzschentke@gmail.com>
2026-02-23 12:04:43 +0100

    fixed merge issue

--- lang/core_lang/src/syntax/program.rs
+++ lang/core_lang/src/syntax/program.rs
@@ -48,7 +48,7 @@ impl Prog {
             .map(|mut def| {
                 def.body = def
                     .body
-                    .uniquify(&mut def.context.vars(), &mut def.used_vars);
+                    .uniquify(&mut UniquifyState::new(def.context.vars()));
                 def
             })
             .collect();
