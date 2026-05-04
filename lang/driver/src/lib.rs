a9944c0fb6bf974fb874072d45d0f753014123ec
IchgebBauchkanon <mullerdominik594@gmail.com>
2026-05-04 15:00:26 +0200

    deleted debug prints

--- lang/driver/src/lib.rs
+++ lang/driver/src/lib.rs
@@ -118,7 +118,8 @@ impl Driver {
         let loaded = load_module(&parsed, path, parent_decl, visited, Driver::create_driver)?;
 
         self.loaded.insert(path.clone(), loaded.clone());
-        //println!("{:?}", loaded.imports);
+        //println!("{:?}", path.file_name().expect("Should have filename"));
+        //println!("{:?}", loaded.imports.keys());
         Ok(loaded)
     }
 
